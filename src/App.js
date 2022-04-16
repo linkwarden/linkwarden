0dbb0c86456f1731e1e98191ed5028d9962a8471
${NEW_NAME} <${NEW_EMAIL}>
2022-04-16 22:20:42 +0430

    base frontend w/ table

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import List from './componets/List';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch('https://api.github.com/users');
+      const resJSON = await res.json();
+      // data.sort((b, a) => new Date(b.Date) - new Date(a.Date)) // Sort by date
+      setData(resJSON);
+    }
+
+    fetchData();
+  }, []);
+  
   return (
     <div className="App">
       <div className="head">
         <input className="search" type="search" placeholder="Search bookmarks"/>
-        <button className="search-btn"><span class="material-icons-outlined md-36">search</span></button>
-        <button className="add-btn"><span class="material-icons-outlined md-36">add</span></button>
-        <button className="settings-btn"><span class="material-icons-outlined md-36">settings</span></button>
+        <button className="search-btn"><span className="material-icons-outlined md-36">search</span></button>
+        <button className="add-btn"><span className="material-icons-outlined md-36">add</span></button>
+        <button className="settings-btn"><span className="material-icons-outlined md-36">settings</span></button>
       </div>
-      <div className="lables">
-        <p id="id">#</p>
-        <p className="lable">Name</p>
-        <p className="lable">Title</p>
-        <p className="lable">Link</p>
-      </div>
+      <List data={data} />
     </div>
   );
 }
