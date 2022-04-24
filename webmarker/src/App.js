432a35ac93fa31382ffb326e78aac8a57d6ed67a
spiicytuna <334199+spiicytuna@users.noreply.github.com>
2022-04-25 04:04:30 +0430

    added search functionality

--- webmarker/src/App.js
+++ webmarker/src/App.js
@@ -6,10 +6,19 @@ import AddModal from './componets/AddModal';
 function App() {
   const [data, setData] = useState([]);
   const [isAdding, setIsAdding] = useState(false);
+  const [searchQuery, setSearchQuery] = useState('');
 
   function exitAdding() {
     setIsAdding(!isAdding)
   }
+
+  function search(e) {
+    setSearchQuery(e.target.value);
+  }
+  
+  const filteredData = data.filter((e) => {
+    return (e.name.toLowerCase().includes(searchQuery.toLowerCase()) || e.title.toLowerCase().includes(searchQuery.toLowerCase()) || e.tag.toLowerCase().includes(searchQuery.toLowerCase()))
+  });
 
   useEffect(() => {
     async function fetchData() {
@@ -25,12 +34,11 @@ function App() {
   return (
     <div className="App">
       <div className="head">
-        <input className="search" type="search" placeholder="Search bookmarks"/>
-        <button className="search-btn"><span className="material-icons-outlined md-36">search</span></button>
+        <input className="search" type="search" placeholder="Search for name/title/tag" onChange={search}/>
         <button className="add-btn"><span className="material-icons-outlined md-36" onClick={() => setIsAdding(true)}>add</span></button>
         <button className="settings-btn"><span className="material-icons-outlined md-36">settings</span></button>
       </div>
-      <List data={data} />
+      <List data={filteredData} />
       {isAdding ? <AddModal onExit={exitAdding} /> : null}
     </div>
   );
