43251a85d52682bc019d95643635c8780fb8e15f
Daniel <daniel31x13@gmail.com>
2022-05-30 21:14:34 +0430

    Finalized filters & Tags + UI changes

--- src/componets/TagSelection.js
+++ src/componets/TagSelection.js
@@ -8,12 +8,6 @@ const options = [
 ];
 
 const customStyles = {
-  container: (provided) => ({
-    ...provided,
-    marginLeft: '20%',
-    marginRight: '20%',
-  }),
-
   placeholder: (provided) => ({
     ...provided,
     color: '#a9a9a9',
@@ -23,15 +17,14 @@ const customStyles = {
     ...provided,
     display: 'none',
   }),
-  
+
   menu: (provided) => ({
     ...provided,
     padding: '5px',
-    borderRadius: '10px',
     opacity: '90%',
     color: 'gray',
     background: '#273949',
-    boxShadow: 'rgba(0, 0, 0, 0.4) 0px 2px 4px, rgba(0, 0, 0, 0.3) 0px 7px 13px -3px, rgba(0, 0, 0, 0.2) 0px -3px 0px inset',
+    boxShadow: 'rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px',
   }),
 
   input: (provided) => ({
@@ -39,25 +32,25 @@ const customStyles = {
     color: 'white',
 }),
 
-  control: (provided) => ({
+  control: (provided, state) => ({
     ...provided,
     background: '#273949',
     border: 'none',
-    borderRadius: '10px',
-    boxShadow: 'rgba(0, 0, 0, 0.4) 0px 2px 4px, rgba(0, 0, 0, 0.3) 0px 7px 13px -3px, rgba(0, 0, 0, 0.2) 0px -3px 0px inset',
+    boxShadow: state.isFocused ? 'rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px' : 'rgba(0, 0, 0, 0.4) 0px 2px 4px, rgba(0, 0, 0, 0.3) 0px 7px 13px -3px, rgba(0, 0, 0, 0.2) 0px -3px 0px inset',
   }),
 }
 
-export default function TagSelection() {
-  const [selectedOption, setSelectedOption] = useState(null);
+export default function TagSelection({setTags, tags}) {
+  const data = tags().map((e) => {
+      return { value: e, label: e }
+  })
 
   return (
     <CreatableSelect
         styles={customStyles}
         isMulti
-        defaultValue={selectedOption}
-        onChange={setSelectedOption}
-        options={options}
+        onChange={setTags}
+        options={data}
     />
   );
 }
