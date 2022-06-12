b0618525ad60e0af9398ea2d706fc370ccedb470
${NEW_NAME} <${NEW_EMAIL}>
2022-06-12 07:18:12 +0430

    Added timestamp to each bookmark + Bug fix.

--- src/modules/sortList.js
+++ src/modules/sortList.js
@@ -1,9 +1,15 @@
 const sortList = (data, sortBy) => {
     let sortedData = data;
-    if(sortBy === 'Date (Oldest first)') {
-      sortedData.reverse();
+    if(sortBy === 'Default') {
+      sortedData.sort((a, b) => {
+        return new Date(b.date) - new Date(a.date);
+      });
+    } else if(sortBy === 'Date (Oldest first)') {
+      sortedData.sort((a,b) => {
+        return new Date(a.date) - new Date(b.date);
+      });
     } else if(sortBy === 'Name (A-Z)') {
-      sortedData.sort(function(a, b){
+      sortedData.sort((a, b) => {
         const A = a.name.toLowerCase(), B = b.name.toLowerCase();
         if (A < B)
          return -1;
@@ -12,7 +18,7 @@ const sortList = (data, sortBy) => {
         return 0;
        });
     } else if(sortBy === 'Name (Z-A)') {
-      sortedData.sort(function(a, b){
+      sortedData.sort((a, b) => {
         const A = a.name.toLowerCase(), B = b.name.toLowerCase();
         if (A > B)
          return -1;
@@ -21,7 +27,7 @@ const sortList = (data, sortBy) => {
         return 0;
        });
     } else if(sortBy === 'Title (A-Z)') {
-      sortedData.sort(function(a, b){
+      sortedData.sort((a, b) => {
         const A = a.title.toLowerCase(), B = b.title.toLowerCase();
         if (A < B)
          return -1;
@@ -30,7 +36,7 @@ const sortList = (data, sortBy) => {
         return 0;
        });
     } else if(sortBy === 'Title (Z-A)') {
-      sortedData.sort(function(a, b){
+      sortedData.sort((a, b) => {
         const A = a.title.toLowerCase(), B = b.title.toLowerCase();
         if (A > B)
          return -1;
