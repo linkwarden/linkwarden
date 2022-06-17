d28df3a061d9fcdd7369da36d8d890bf9fcd96ad
Daniel <daniel31x13@gmail.com>
2022-06-17 14:08:48 +0430

    Much neater filters + cleaner code.

--- src/modules/filterData.js
+++ src/modules/filterData.js
@@ -1,28 +1,26 @@
 const filteredData = (
   data,
   searchQuery,
-  nameChecked,
-  tagsChecked,
-  descriptionChecked
+  filterCheckbox
 ) => {
   return data.filter((e) => {
     const name = e.name.toLowerCase().includes(searchQuery.toLowerCase());
     const title = e.title.toLowerCase().includes(searchQuery.toLowerCase());
     const tags = e.tag.some((e) => e.includes(searchQuery.toLowerCase()));
 
-    if (nameChecked && tagsChecked && descriptionChecked) {
+    if (filterCheckbox === [true, true, true]) {
       return name || title || tags;
-    } else if (nameChecked && tagsChecked) {
+    } else if (filterCheckbox[0] && filterCheckbox[2]) {
       return name || tags;
-    } else if (nameChecked && descriptionChecked) {
+    } else if (filterCheckbox[0] && filterCheckbox[1]) {
       return name || title;
-    } else if (tagsChecked && descriptionChecked) {
+    } else if (filterCheckbox[2] && filterCheckbox[1]) {
       return tags || title;
-    } else if (nameChecked) {
+    } else if (filterCheckbox[0]) {
       return name;
-    } else if (tagsChecked) {
+    } else if (filterCheckbox[1]) {
       return tags;
-    } else if (descriptionChecked) {
+    } else if (filterCheckbox[2]) {
       return title;
     }
   });
