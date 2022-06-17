0b269f92405bb37cfab49ec292eefe7d707ea1b1
${NEW_NAME} <${NEW_EMAIL}>
2022-06-17 14:08:48 +0430

    Much neater filters + cleaner code.

--- src/componets/Filters.js
+++ src/componets/Filters.js
@@ -1,18 +1,31 @@
 import "../styles/Filters.css";
+import { useState } from "react";
 
 const Filters = ({
-  nameChecked,
-  handleNameCheckbox,
-  descriptionChecked,
-  handleDescriptionCheckbox,
-  tagsChecked,
-  handleTagsCheckbox,
+  filterCheckbox,
+  handleFilterCheckbox,
+  sortBy,
+  sort,
   onExit,
 }) => {
+  const [nameChecked, setNameChecked] = useState(filterCheckbox[0]),
+    [titleChecked, setTitleChecked] = useState(filterCheckbox[1]),
+    [tagChecked, setTagChecked] = useState(filterCheckbox[2]),
+    [radio, setRadio] = useState(sort);
+
   function abort(e) {
     if (e.target.className === "filter-overlay") {
       onExit();
     }
+  }
+
+  function handleRadio(e) {
+    setRadio(e.target.value);
+  }
+
+  function applyChanges() {
+    handleFilterCheckbox([nameChecked, titleChecked, tagChecked]);
+    sortBy(radio);
   }
 
   return (
@@ -20,31 +33,105 @@ const Filters = ({
       <div className="filter-overlay" onClick={abort}></div>
       <div className="filter-box">
         <fieldset className="filter">
-          <legend>Filter by</legend>
-          <label>
-            <input
-              type="checkbox"
-              checked={nameChecked}
-              onChange={handleNameCheckbox}
-            />
-            Name
-          </label>
-          <label>
-            <input
-              type="checkbox"
-              checked={descriptionChecked}
-              onChange={handleDescriptionCheckbox}
-            />
-            Website title
-          </label>
-          <label>
-            <input
-              type="checkbox"
-              checked={tagsChecked}
-              onChange={handleTagsCheckbox}
-            />
-            Tags
-          </label>
+          <legend>Filter search</legend>
+
+          <div className="filter-groups">
+            <div className="section">
+              <h4>Sort by</h4>
+              <label>
+                <input
+                  name="sort"
+                  checked={radio == 1}
+                  onChange={handleRadio}
+                  type="radio"
+                  value={1}
+                />
+                &#xf271; Date (Newest first)
+              </label>
+              <label>
+                <input
+                  name="sort"
+                  checked={radio == 2}
+                  onChange={handleRadio}
+                  type="radio"
+                  value={2}
+                />
+                &#xf272; Date (Oldest first)
+              </label>
+              <label>
+                <input
+                  name="sort"
+                  checked={radio == 3}
+                  onChange={handleRadio}
+                  type="radio"
+                  value={3}
+                />
+                &#xf15d; Name (A-Z)
+              </label>
+              <label>
+                <input
+                  name="sort"
+                  checked={radio == 4}
+                  onChange={handleRadio}
+                  type="radio"
+                  value={4}
+                />
+                &#xf15e; Name (Z-A)
+              </label>
+              <label>
+                <input
+                  name="sort"
+                  checked={radio == 5}
+                  onChange={handleRadio}
+                  type="radio"
+                  value={5}
+                />
+                &#xf15d; Website title (A-Z)
+              </label>
+              <label>
+                <input
+                  name="sort"
+                  checked={radio == 6}
+                  onChange={handleRadio}
+                  type="radio"
+                  value={6}
+                />
+                &#xf15e; Website title (Z-A)
+              </label>
+            </div>
+
+            <div className="section">
+              <h4>Include/Exclude</h4>
+              <label>
+                <input
+                  type="checkbox"
+                  checked={nameChecked}
+                  onChange={() => setNameChecked(!nameChecked)}
+                />
+                Name
+              </label>
+              <label>
+                <input
+                  type="checkbox"
+                  checked={titleChecked}
+                  onChange={() => setTitleChecked(!titleChecked)}
+                />
+                Website title
+              </label>
+              <label>
+                <input
+                  type="checkbox"
+                  checked={tagChecked}
+                  onChange={() => setTagChecked(!tagChecked)}
+                />
+                Tags
+              </label>
+            </div>
+          </div>
+
+          <button className="apply-btn" onClick={applyChanges}>
+            Apply
+          </button>
         </fieldset>
       </div>
     </>
--- src/modules/sortList.js
+++ src/modules/sortList.js
@@ -1,14 +1,14 @@
 const sortList = (data, sortBy) => {
   let sortedData = data;
-  if (sortBy === "Default") {
+  if (sortBy == 1) {
     sortedData.sort((a, b) => {
       return new Date(b.date) - new Date(a.date);
     });
-  } else if (sortBy === "Date (Oldest first)") {
+  } else if (sortBy == 2) {
     sortedData.sort((a, b) => {
       return new Date(a.date) - new Date(b.date);
     });
-  } else if (sortBy === "Name (A-Z)") {
+  } else if (sortBy == 3) {
     sortedData.sort((a, b) => {
       const A = a.name.toLowerCase(),
         B = b.name.toLowerCase();
@@ -16,7 +16,7 @@ const sortList = (data, sortBy) => {
       if (A > B) return 1;
       return 0;
     });
-  } else if (sortBy === "Name (Z-A)") {
+  } else if (sortBy == 4) {
     sortedData.sort((a, b) => {
       const A = a.name.toLowerCase(),
         B = b.name.toLowerCase();
@@ -24,7 +24,7 @@ const sortList = (data, sortBy) => {
       if (A < B) return 1;
       return 0;
     });
-  } else if (sortBy === "Title (A-Z)") {
+  } else if (sortBy == 5) {
     sortedData.sort((a, b) => {
       const A = a.title.toLowerCase(),
         B = b.title.toLowerCase();
@@ -32,7 +32,7 @@ const sortList = (data, sortBy) => {
       if (A > B) return 1;
       return 0;
     });
-  } else if (sortBy === "Title (Z-A)") {
+  } else if (sortBy == 6) {
     sortedData.sort((a, b) => {
       const A = a.title.toLowerCase(),
         B = b.title.toLowerCase();
