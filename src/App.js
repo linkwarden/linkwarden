f374292feb541b5c6988b83dff4ba723f7f0a5f2
Daniel <daniel31x13@gmail.com>
2022-06-26 21:10:27 +0430

    Small change.

--- src/App.js
+++ src/App.js
@@ -80,7 +80,7 @@ function App() {
   useEffect(() => {
     const currentURL = new URL(window.location.href);
     SetPath(currentURL.pathname);
-  })
+  }, [path])
 
   useEffect(() => {
     fetchData();
