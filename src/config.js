32a642bb9a6892db687768af91c6fd40a274216c
spiicytuna <334199+spiicytuna@users.noreply.github.com>
2022-07-19 21:23:24 -0400

    Small change.

--- src/config.js
+++ src/config.js
@@ -1,1 +1,1 @@
-export const API_HOST = process.env.REACT_APP_API_HOST || "http://localhost:5000"; // API full address
+export const API_HOST = process.env.REACT_APP_API_HOST || "http://localhost:5500"; // API full address
