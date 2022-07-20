7cd0b9d9560f40ab2183e4e08e75e0037027eda9
Daniel31x13 <daniel31x13@gmail.com>
2022-07-19 21:19:40 -0400

    Improved README + Easier configuration.

--- api/config.js
+++ api/config.js
@@ -1,4 +1,4 @@
-module.exports.port = process.env.PORT || 5500;
+module.exports.port = process.env.PORT || 5000;
 module.exports.URI = process.env.MONGODB_URI || "mongodb://localhost:27017"; // URI
 module.exports.database = process.env.DB_NAME || "sample_db"; // Database name
 module.exports.collection = process.env.COLLECTION_NAME || "list"; // Collection name
