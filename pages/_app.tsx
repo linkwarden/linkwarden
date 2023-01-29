4cd0e8799c1c7bcb764ad0132ba0545edbe2982a
Daniel <daniel31x13@gmail.com>
2023-01-29 21:42:36 +0330

    Replaced JavaScript with TypeScript.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
 import Head from "next/head";
 
-export default function Home() {
+export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
@@ -9,9 +11,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <h1 className="text-3xl font-bold text-center">LinkWarden</h1>
-      </main>
+      <Component {...pageProps} />
     </>
   );
 }
