fcbecb5bc577120e6bd9b430c28474bfb7629e06
Daniel <daniel31x13@gmail.com>
2023-05-28 20:52:01 +0330

    bug fixes

--- components/ProfilePhoto.tsx
+++ components/ProfilePhoto.tsx
@@ -15,7 +15,7 @@ export default function ProfilePhoto({ src, className }: Props) {
       className={`h-10 w-10 shadow rounded-full border-[3px] border-sky-100 ${className}`}
     >
       <div
-        className={`text-white bg-sky-500 h-10 w-10 shadow rounded-full border-[3px] border-sky-100 flex items-center justify-center ${className}`}
+        className={`bg-sky-500 text-white h-10 w-10 shadow rounded-full border-[3px] border-sky-100 flex items-center justify-center ${className}`}
       >
         <FontAwesomeIcon icon={faUser} className="w-5 h-5" />
       </div>
