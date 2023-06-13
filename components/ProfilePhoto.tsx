99cfbdf44bee3fcb76aa06389623bcc49908fb9f
Daniel <daniel31x13@gmail.com>
2023-06-13 22:49:37 +0330

    finalized dashboard

--- components/ProfilePhoto.tsx
+++ components/ProfilePhoto.tsx
@@ -33,9 +33,9 @@ export default function ProfilePhoto({
 
   return error || !src ? (
     <div
-      className={`bg-sky-500 text-white h-10 w-10 shadow rounded-full border-[3px] border-slate-200 flex items-center justify-center ${className}`}
+      className={`bg-sky-500 text-white h-10 w-10 aspect-square shadow rounded-full border-[3px] border-slate-200 flex items-center justify-center ${className}`}
     >
-      <FontAwesomeIcon icon={faUser} className="w-1/2 h-1/2" />
+      <FontAwesomeIcon icon={faUser} className="w-1/2 h-1/2 aspect-square" />
     </div>
   ) : (
     <Image
@@ -43,7 +43,7 @@ export default function ProfilePhoto({
       src={src}
       height={112}
       width={112}
-      className={`h-10 w-10 shadow rounded-full border-[3px] border-slate-200 ${className}`}
+      className={`h-10 w-10 shadow rounded-full aspect-square border-[3px] border-slate-200 ${className}`}
     />
   );
 }
--- prisma/migrations/20230613191848_init/migration.sql
+++ prisma/migrations/20230613191848_init/migration.sql
@@ -25,33 +25,6 @@ CREATE TABLE "Collection" (
 );
 
 -- CreateTable
-CREATE TABLE "PinnedCollections" (
-    "id" SERIAL NOT NULL,
-    "userId" INTEGER NOT NULL,
-    "collectionId" INTEGER NOT NULL,
-
-    CONSTRAINT "PinnedCollections_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "PinnedLinks" (
-    "id" SERIAL NOT NULL,
-    "userId" INTEGER NOT NULL,
-    "linkId" INTEGER NOT NULL,
-
-    CONSTRAINT "PinnedLinks_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "PinnedTags" (
-    "id" SERIAL NOT NULL,
-    "userId" INTEGER NOT NULL,
-    "tagId" INTEGER NOT NULL,
-
-    CONSTRAINT "PinnedTags_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
 CREATE TABLE "UsersAndCollections" (
     "userId" INTEGER NOT NULL,
     "collectionId" INTEGER NOT NULL,
@@ -84,6 +57,12 @@ CREATE TABLE "Tag" (
 );
 
 -- CreateTable
+CREATE TABLE "_LinkToUser" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL
+);
+
+-- CreateTable
 CREATE TABLE "_LinkToTag" (
     "A" INTEGER NOT NULL,
     "B" INTEGER NOT NULL
@@ -99,6 +78,12 @@ CREATE UNIQUE INDEX "Collection_name_ownerId_key" ON "Collection"("name", "owner
 CREATE UNIQUE INDEX "Tag_name_ownerId_key" ON "Tag"("name", "ownerId");
 
 -- CreateIndex
+CREATE UNIQUE INDEX "_LinkToUser_AB_unique" ON "_LinkToUser"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_LinkToUser_B_index" ON "_LinkToUser"("B");
+
+-- CreateIndex
 CREATE UNIQUE INDEX "_LinkToTag_AB_unique" ON "_LinkToTag"("A", "B");
 
 -- CreateIndex
@@ -106,24 +91,6 @@ CREATE INDEX "_LinkToTag_B_index" ON "_LinkToTag"("B");
 
 -- AddForeignKey
 ALTER TABLE "Collection" ADD CONSTRAINT "Collection_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "PinnedCollections" ADD CONSTRAINT "PinnedCollections_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "PinnedCollections" ADD CONSTRAINT "PinnedCollections_collectionId_fkey" FOREIGN KEY ("collectionId") REFERENCES "Collection"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "PinnedLinks" ADD CONSTRAINT "PinnedLinks_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "PinnedLinks" ADD CONSTRAINT "PinnedLinks_linkId_fkey" FOREIGN KEY ("linkId") REFERENCES "Link"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "PinnedTags" ADD CONSTRAINT "PinnedTags_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "PinnedTags" ADD CONSTRAINT "PinnedTags_tagId_fkey" FOREIGN KEY ("tagId") REFERENCES "Tag"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "UsersAndCollections" ADD CONSTRAINT "UsersAndCollections_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
@@ -136,6 +103,12 @@ ALTER TABLE "Link" ADD CONSTRAINT "Link_collectionId_fkey" FOREIGN KEY ("collect
 
 -- AddForeignKey
 ALTER TABLE "Tag" ADD CONSTRAINT "Tag_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_LinkToUser" ADD CONSTRAINT "_LinkToUser_A_fkey" FOREIGN KEY ("A") REFERENCES "Link"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_LinkToUser" ADD CONSTRAINT "_LinkToUser_B_fkey" FOREIGN KEY ("B") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "_LinkToTag" ADD CONSTRAINT "_LinkToTag_A_fkey" FOREIGN KEY ("A") REFERENCES "Link"("id") ON DELETE CASCADE ON UPDATE CASCADE;
