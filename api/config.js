43f98ea78fac3302f93676ec682eeed14507f229
spiicytuna <334199+spiicytuna@users.noreply.github.com>
2023-06-29 05:27:56 +0330

    deleted old files

--- api/config.js
+++
@@ -1,7 +0,0 @@
-module.exports.port = process.env.PORT || 5500;
-module.exports.URI = process.env.MONGODB_URI || "mongodb://localhost:27017"; // URI
-module.exports.database = process.env.DB_NAME || "sample_db"; // Database name
-module.exports.collection = process.env.COLLECTION_NAME || "list"; // Collection name
-const storageLocation = process.env.STORAGE_LOCATION || "./media";
-module.exports.screenshotDirectory = storageLocation + "/screenshots";
-module.exports.pdfDirectory = storageLocation + "/pdfs";
