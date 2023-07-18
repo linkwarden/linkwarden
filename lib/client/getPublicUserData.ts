714d3d283a118ffdf5102ff3f06c20c5f84bc320
${NEW_NAME} <${NEW_EMAIL}>
2023-07-17 23:31:26 -0400

    collection owner is now visible to other users

---
+++ lib/client/getPublicUserData.ts
@@ -0,0 +1,21 @@
+import { toast } from "react-hot-toast";
+
+export default async function getPublicUserData({
+  username,
+  id,
+}: {
+  username?: string;
+  id?: number;
+}) {
+  const response = await fetch(
+    `/api/routes/users?id=${id}&${
+      username ? `username=${username?.toLowerCase()}` : undefined
+    }`
+  );
+
+  const data = await response.json();
+
+  if (!response.ok) toast.error(data.response);
+
+  return data.response;
+}
--- pages/api/routes/users/index.ts
+++ pages/api/routes/users/index.ts
@@ -15,14 +15,29 @@ export default async function users(req: NextApiRequest, res: NextApiResponse) {
         "You are not a subscriber, feel free to reach out to us at hello@linkwarden.app in case of any issues.",
     });
 
-  const lookupUsername = req.query.username as string;
+  const lookupUsername = (req.query.username as string) || undefined;
+  const lookupId = Number(req.query.id) || undefined;
   const isSelf = session.user.username === lookupUsername ? true : false;
 
   if (req.method === "GET") {
-    const users = await getUsers(lookupUsername, isSelf, session.user.username);
+    const users = await getUsers({
+      params: {
+        lookupUsername,
+        lookupId,
+      },
+      isSelf,
+      username: session.user.username,
+    });
     return res.status(users.status).json({ response: users.response });
   } else if (req.method === "PUT" && !req.body.password) {
     const updated = await updateUser(req.body, session.user);
     return res.status(updated.status).json({ response: updated.response });
   }
 }
+
+// {
+//   lookupUsername,
+//   lookupId,
+// },
+// isSelf,
+// session.user.username
