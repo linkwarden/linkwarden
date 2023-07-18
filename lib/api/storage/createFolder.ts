c5194a6bddf0865d76066fd6c8b08484d81a5042
spiicytuna <334199+spiicytuna@users.noreply.github.com>
2023-07-18 11:59:57 -0400

    small improvements

--- lib/api/storage/createFolder.ts
+++ lib/api/storage/createFolder.ts
@@ -4,7 +4,7 @@ import s3Client from "./s3Client";
 
 export default function createFolder({ filePath }: { filePath: string }) {
   if (s3Client) {
-    // Do nothing, S3 builds files recursively
+    // Do nothing, S3 creates directories recursively
   } else {
     const storagePath = process.env.STORAGE_FOLDER;
     const creationPath = path.join(process.cwd(), storagePath + "/" + filePath);
