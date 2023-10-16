943a17654750c3cacb4480be9b2e835386b7ebb5
spiicytuna <334199+spiicytuna@users.noreply.github.com>
2023-10-16 13:10:52 -0400

    fixed filter by tags + refactored search + bug fixed + settings page [WIP]

--- lib/client/avatarExists.ts
+++ lib/client/avatarExists.ts
@@ -1,4 +1,13 @@
+const avatarCache = new Map();
+
 export default async function avatarExists(fileUrl: string): Promise<boolean> {
+  if (avatarCache.has(fileUrl)) {
+    return avatarCache.get(fileUrl);
+  }
+
   const response = await fetch(fileUrl, { method: "HEAD" });
-  return !(response.headers.get("content-type") === "text/html");
+  const exists = !(response.headers.get("content-type") === "text/html");
+
+  avatarCache.set(fileUrl, exists);
+  return exists;
 }
