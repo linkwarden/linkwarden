c28107f3b2fd426972e572c07c4d5da9ca3b34f9
spiicytuna <334199+spiicytuna@users.noreply.github.com>
2023-10-27 16:06:42 -0400

    updated prisma schema

---
+++ prisma/migrations/20231027195438_remove_unnecessary_tables/migration.sql
@@ -0,0 +1,18 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Account` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Session` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Account" DROP CONSTRAINT "Account_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Session" DROP CONSTRAINT "Session_userId_fkey";
+
+-- DropTable
+DROP TABLE "Account";
+
+-- DropTable
+DROP TABLE "Session";
