ffc927759e19b20842536b91b94153817670f267
Sebastian Hierholzer <sh@hierholzer.dev>
2023-12-03 21:01:28 +0100

    feat: refactored login

--- lib/api/controllers/users/userId/updateUserById.ts
+++ lib/api/controllers/users/userId/updateUserById.ts
@@ -13,45 +13,88 @@ export default async function updateUserById(
   userId: number,
   data: AccountSettings
 ) {
-  if (emailEnabled && !data.email)
-    return {
-      response: "Email invalid.",
-      status: 400,
-    };
-  else if (!data.username)
-    return {
-      response: "Username invalid.",
-      status: 400,
-    };
-  if (data.newPassword && data.newPassword?.length < 8)
-    return {
-      response: "Password must be at least 8 characters.",
-      status: 400,
-    };
-
-  // Check email (if enabled)
-  const checkEmail =
-    /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-  if (emailEnabled && !checkEmail.test(data.email?.toLowerCase() || ""))
-    return {
-      response: "Please enter a valid email.",
-      status: 400,
-    };
-
-  const checkUsername = RegExp("^[a-z0-9_-]{3,31}$");
-
-  if (!checkUsername.test(data.username.toLowerCase()))
-    return {
-      response:
-        "Username has to be between 3-30 characters, no spaces and special characters are allowed.",
-      status: 400,
-    };
-
-  const userIsTaken = await prisma.user.findFirst({
+  const ssoUser = await prisma.account.findFirst({
     where: {
-      id: { not: userId },
-      OR: emailEnabled
-        ? [
+      userId: userId,
+    },
+  });
+  const user = await prisma.user.findUnique({
+    where: {
+      id: userId,
+    },
+  });
+  if (ssoUser) { // deny changes to SSO-defined properties
+    if (data.email !== user?.email) {
+      return {
+        response: "SSO users cannot change their email.",
+        status: 400,
+      };
+    }
+    if (data.newPassword) {
+      return {
+        response: "SSO Users cannot change their password.",
+        status: 400,
+      };
+    }
+    if (data.name !== user?.name) {
+      return {
+        response: "SSO Users cannot change their name.",
+        status: 400,
+      };
+    }
+    if (data.username !== user?.username) {
+      return {
+        response: "SSO Users cannot change their username.",
+        status: 400,
+      };
+    }
+    if (data.image !== "") {
+      return {
+        response: "SSO Users cannot change their avatar.",
+        status: 400,
+      };
+    }
+
+  } else { // verify only for non-SSO users
+    // SSO users cannot change their email, password, name, username, or avatar
+    if (emailEnabled && !data.email)
+      return {
+        response: "Email invalid.",
+        status: 400,
+      };
+    else if (!data.username)
+      return {
+        response: "Username invalid.",
+        status: 400,
+      };
+    if (data.newPassword && data.newPassword?.length < 8)
+      return {
+        response: "Password must be at least 8 characters.",
+        status: 400,
+      };
+    // Check email (if enabled)
+    const checkEmail =
+      /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    if (emailEnabled && !checkEmail.test(data.email?.toLowerCase() || ""))
+      return {
+        response: "Please enter a valid email.",
+        status: 400,
+      };
+
+    const checkUsername = RegExp("^[a-z0-9_-]{3,31}$");
+
+    if (!checkUsername.test(data.username.toLowerCase()))
+      return {
+        response:
+          "Username has to be between 3-30 characters, no spaces and special characters are allowed.",
+        status: 400,
+      };
+
+    const userIsTaken = await prisma.user.findFirst({
+      where: {
+        id: { not: userId },
+        OR: emailEnabled
+          ? [
             {
               username: data.username.toLowerCase(),
             },
@@ -59,65 +102,69 @@ export default async function updateUserById(
               email: data.email?.toLowerCase(),
             },
           ]
-        : [
+          : [
             {
               username: data.username.toLowerCase(),
             },
           ],
-    },
-  });
+      },
+    });
 
-  if (userIsTaken) {
-    if (data.email?.toLowerCase().trim() === userIsTaken.email?.trim())
+    if (userIsTaken) {
+      if (data.email?.toLowerCase().trim() === userIsTaken.email?.trim())
+        return {
+          response: "Email is taken.",
+          status: 400,
+        };
+      else if (
+        data.username?.toLowerCase().trim() === userIsTaken.username?.trim()
+      )
+        return {
+          response: "Username is taken.",
+          status: 400,
+        };
+
       return {
-        response: "Email is taken.",
-        status: 400,
-      };
-    else if (
-      data.username?.toLowerCase().trim() === userIsTaken.username?.trim()
-    )
-      return {
-        response: "Username is taken.",
-        status: 400,
-      };
-
-    return {
-      response: "Username/Email is taken.",
-      status: 400,
-    };
-  }
-
-  // Avatar Settings
-
-  if (data.image?.startsWith("data:image/jpeg;base64")) {
-    if (data.image.length < 1572864) {
-      try {
-        const base64Data = data.image.replace(/^data:image\/jpeg;base64,/, "");
-
-        createFolder({ filePath: `uploads/avatar` });
-
-        await createFile({
-          filePath: `uploads/avatar/${userId}.jpg`,
-          data: base64Data,
-          isBase64: true,
-        });
-      } catch (err) {
-        console.log("Error saving image:", err);
-      }
-    } else {
-      console.log("A file larger than 1.5MB was uploaded.");
-      return {
-        response: "A file larger than 1.5MB was uploaded.",
+        response: "Username/Email is taken.",
         status: 400,
       };
     }
-  } else if (data.image == "") {
-    removeFile({ filePath: `uploads/avatar/${userId}.jpg` });
+
+    // Avatar Settings
+
+    if (data.image?.startsWith("data:image/jpeg;base64")) {
+      if (data.image.length < 1572864) {
+        try {
+          const base64Data = data.image.replace(/^data:image\/jpeg;base64,/, "");
+
+          createFolder({ filePath: `uploads/avatar` });
+
+          await createFile({
+            filePath: `uploads/avatar/${userId}.jpg`,
+            data: base64Data,
+            isBase64: true,
+          });
+        } catch (err) {
+          console.log("Error saving image:", err);
+        }
+      } else {
+        console.log("A file larger than 1.5MB was uploaded.");
+        return {
+          response: "A file larger than 1.5MB was uploaded.",
+          status: 400,
+        };
+      }
+    } else if (data.image == "") {
+      removeFile({ filePath: `uploads/avatar/${userId}.jpg` });
+    }
   }
 
   const previousEmail = (
     await prisma.user.findUnique({ where: { id: userId } })
   )?.email;
+
+
+
 
   // Other settings
 
@@ -130,7 +177,7 @@ export default async function updateUserById(
     },
     data: {
       name: data.name,
-      username: data.username.toLowerCase().trim(),
+      username: data.username?.toLowerCase().trim(),
       email: data.email?.toLowerCase().trim(),
       isPrivate: data.isPrivate,
       image: data.image ? `uploads/avatar/${userId}.jpg` : "",
