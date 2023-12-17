8952f730aebbb426e7defb4a698aed9806fab269
spiicytuna <334199+spiicytuna@users.noreply.github.com>
2023-12-18 06:06:55 +0800

    minor fix

--- components/LinkViews/GridView.tsx
+++ components/LinkViews/GridView.tsx
@@ -1,5 +1,5 @@
 import LinkCardGrid from "@/components/LinkViews/LinkComponents/LinkCardGrid";
-import { LinkIncludingShortenedCollectionAndTags } from "@/types/global";
+import {LinkIncludingShortenedCollectionAndTags} from "@/types/global";
 
 export default function CompactGridView({
   links,
@@ -10,8 +10,8 @@ export default function CompactGridView({
     <div className="columns-1 xl:columns-2 2xl:columns-3 gap-5">
       {links.map((e, i) => {
         return (
-          <div className="break-inside-avoid mb-5">
-            <LinkCardGrid key={i} link={e} count={i} />
+          <div key={i} className="break-inside-avoid mb-5">
+            <LinkCardGrid link={e} count={i}/>
           </div>
         );
       })}
