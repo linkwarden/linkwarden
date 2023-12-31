4e293304720029a463a6f1ad1f66be5fe00bf2de
daniel31x13 <daniel31x13@gmail.com>
2023-12-31 10:46:09 -0500

    added an extra environment variable

--- lib/api/validateUrlSize.ts
+++ lib/api/validateUrlSize.ts
@@ -1,6 +1,17 @@
+import fetch from "node-fetch";
+import https from "https";
+
 export default async function validateUrlSize(url: string) {
   try {
-    const response = await fetch(url, { method: "HEAD" });
+    const httpsAgent = new https.Agent({
+      rejectUnauthorized:
+        process.env.IGNORE_UNAUTHORIZED_CA === "true" ? false : true,
+    });
+
+    const response = await fetch(url, {
+      method: "HEAD",
+      agent: httpsAgent,
+    });
 
     const totalSizeMB =
       Number(response.headers.get("content-length")) / Math.pow(1024, 2);
