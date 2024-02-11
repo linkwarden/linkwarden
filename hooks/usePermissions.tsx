06dfc0adbc2b9cd9b2908c969fa1734139e060b4
spiicytuna <334199+spiicytuna@users.noreply.github.com>
2024-02-10 23:40:26 -0600

    Use memoization for permission checking

--- hooks/usePermissions.tsx
+++ hooks/usePermissions.tsx
@@ -1,15 +1,13 @@
 import useAccountStore from "@/store/account";
 import useCollectionStore from "@/store/collections";
 import { Member } from "@/types/global";
-import { useEffect, useState } from "react";
+import { useMemo } from "react";
 
 export default function usePermissions(collectionId: number) {
   const { collections } = useCollectionStore();
-
   const { account } = useAccountStore();
 
-  const [permissions, setPermissions] = useState<Member | true>();
-  useEffect(() => {
+  const permissions = useMemo(() => {
     const collection = collections.find((e) => e.id === collectionId);
 
     if (collection) {
@@ -21,10 +19,11 @@ export default function usePermissions(collectionId: number) {
         getPermission?.canCreate === false &&
         getPermission?.canUpdate === false &&
         getPermission?.canDelete === false
-      )
+      ) {
         getPermission = undefined;
+      }
 
-      setPermissions(account.id === collection.ownerId || getPermission);
+      return account.id === collection.ownerId || getPermission;
     }
   }, [account, collections, collectionId]);
 
