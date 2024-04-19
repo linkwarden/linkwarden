797075c5f116ad8e72c0ad79406071c97fcf046e
${NEW_NAME} <${NEW_EMAIL}>
2024-04-19 06:16:11 -0400

    code cleanup

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
+const { version } = require("./package.json");
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ["t2.gstatic.com"],
     minimumCacheTTL: 10,
+  },
+  env: {
+    version,
   },
 };
 
