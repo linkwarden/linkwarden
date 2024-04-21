cd09843b9984eac0dde38c127febc19a158ec071
QAComet <lucas@qacomet.com>
2024-04-21 17:15:27 -0600

    feat(e2e): add data-testids to components

--- layouts/CenteredForm.tsx
+++ layouts/CenteredForm.tsx
@@ -6,13 +6,21 @@ import React, { ReactNode, useEffect } from "react";
 interface Props {
   text?: string;
   children: ReactNode;
+  "data-testid"?: string;
 }
 
-export default function CenteredForm({ text, children }: Props) {
+export default function CenteredForm({
+  text,
+  children,
+  "data-testid": dataTestId,
+}: Props) {
   const { settings } = useLocalSettingsStore();
 
   return (
-    <div className="absolute top-0 bottom-0 left-0 right-0 flex justify-center items-center p-5">
+    <div
+      className="absolute top-0 bottom-0 left-0 right-0 flex justify-center items-center p-5"
+      data-testid={dataTestId}
+    >
       <div className="m-auto flex flex-col gap-2 w-full">
         {settings.theme ? (
           <Image
