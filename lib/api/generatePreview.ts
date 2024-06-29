a1471675875cd2fa0e07a3c1da08acdd4a63500f
${NEW_NAME} <${NEW_EMAIL}>
2024-06-29 17:54:31 -0400

    bug fixed

--- lib/api/generatePreview.ts
+++ lib/api/generatePreview.ts
@@ -8,33 +8,39 @@ const generatePreview = async (
   linkId: number
 ) => {
   if (buffer && collectionId && linkId) {
-    // Load the image using Jimp
-    await Jimp.read(buffer, async (err, image) => {
-      if (image && !err) {
-        image?.resize(1280, Jimp.AUTO).quality(20);
-        const processedBuffer = await image?.getBufferAsync(Jimp.MIME_JPEG);
+    try {
+      const image = await Jimp.read(buffer);
 
-        if (
-          Buffer.byteLength(processedBuffer) >
-          1024 * 1024 * Number(process.env.PREVIEW_MAX_BUFFER || 0.1)
-        )
-          return console.log("Error generating preview: Buffer size exceeded");
-
-        await createFile({
-          data: processedBuffer,
-          filePath: `archives/preview/${collectionId}/${linkId}.jpeg`,
-        });
-
-        await prisma.link.update({
-          where: { id: linkId },
-          data: {
-            preview: `archives/preview/${collectionId}/${linkId}.jpeg`,
-          },
-        });
+      if (!image) {
+        console.log("Error generating preview: Image not found");
+        return;
       }
-    }).catch((err) => {
+
+      image.resize(1280, Jimp.AUTO).quality(20);
+      const processedBuffer = await image.getBufferAsync(Jimp.MIME_JPEG);
+
+      if (
+        Buffer.byteLength(processedBuffer) >
+        1024 * 1024 * Number(process.env.PREVIEW_MAX_BUFFER || 0.1)
+      ) {
+        console.log("Error generating preview: Buffer size exceeded");
+        return;
+      }
+
+      await createFile({
+        data: processedBuffer,
+        filePath: `archives/preview/${collectionId}/${linkId}.jpeg`,
+      });
+
+      await prisma.link.update({
+        where: { id: linkId },
+        data: {
+          preview: `archives/preview/${collectionId}/${linkId}.jpeg`,
+        },
+      });
+    } catch (err) {
       console.error("Error processing the image:", err);
-    });
+    }
   }
 };
 
