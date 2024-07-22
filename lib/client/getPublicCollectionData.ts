7d43ed52a4fe0dc32e1cfe999ebe693aac6cd1cc
Isaac Wise <iamisaacwise2006@gmail.com>
2024-07-22 17:50:24 -0500

    format

--- lib/client/getPublicCollectionData.ts
+++ lib/client/getPublicCollectionData.ts
@@ -9,7 +9,8 @@ const getPublicCollectionData = async (
 ) => {
   const res = await fetch("/api/v1/public/collections/" + collectionId);
 
-  if (res.status === 400) return { response: "Collection not found.", status: 400 };
+  if (res.status === 400)
+    return { response: "Collection not found.", status: 400 };
 
   const data = await res.json();
 
