04d2b3c6b25374cb3eaa7956b29c0972dbf61809
shichen437 <shichen437@gmail.com>
2024-08-15 17:20:46 +0800

    feat(lang): add chinese translate

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   i18n: {
     defaultLocale: "en",
-    locales: ["en", "it", "fr"],
+    locales: ["en", "it", "fr", "zh"],
   },
   reloadOnPrerender: process.env.NODE_ENV === "development",
 };
