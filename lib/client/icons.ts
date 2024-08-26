9ae9c7c81ab348c32eb80dba4647a93c0e4a12a3
daniel31x13 <daniel31x13@gmail.com>
2024-08-26 18:47:10 -0400

    refactored view dropdown

--- lib/client/icons.ts
+++ lib/client/icons.ts
@@ -11,8 +11,8 @@ export const icons: ReadonlyArray<IconEntry> = iconData.map((entry) => ({
   Icon: Icons[entry.pascal_name as keyof typeof Icons] as Icons.Icon,
 }));
 
-if (process.env.NODE_ENV === "development") {
-  console.log(`${icons.length} icons`);
-}
+// if (process.env.NODE_ENV === "development") {
+//   console.log(`${icons.length} icons`);
+// }
 
 export const iconCount = Intl.NumberFormat("en-US").format(icons.length * 6);
