58d95f12a5318c12d007107c6144619e512214fb
spiicytuna <334199+spiicytuna@users.noreply.github.com>
2024-09-04 22:29:54 -0400

    fix build error

--- components/Icon.tsx
+++ components/Icon.tsx
@@ -13,4 +13,6 @@ const Icon = forwardRef<SVGSVGElement, Props>(({ icon, ...rest }, ref) => {
   } else return <IconComponent ref={ref} {...rest} />;
 });
 
+Icon.displayName = "Icon";
+
 export default Icon;
