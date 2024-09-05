40bb3e6fae4954e264929eae16cafc7edd50f6c3
daniel31x13 <daniel31x13@gmail.com>
2024-09-04 22:29:54 -0400

    fix build error

--- components/IconGrid.tsx
+++ components/IconGrid.tsx
@@ -24,22 +24,26 @@ const IconGrid = ({ query, color, weight, iconName, setIconName }: Props) => {
     return fuse.search(query).map((result) => result.item);
   }, [query]);
 
-  return filteredQueryResultsSelector.map((icon) => {
-    const IconComponent = icon.Icon;
-    return (
-      <div
-        key={icon.pascal_name}
-        onClick={() => setIconName(icon.pascal_name)}
-        className={`cursor-pointer btn p-1 box-border bg-base-100 border-none w-full ${
-          icon.pascal_name === iconName
-            ? "outline outline-1 outline-primary"
-            : ""
-        }`}
-      >
-        <IconComponent size={32} weight={weight} color={color} />
-      </div>
-    );
-  });
+  return (
+    <>
+      {filteredQueryResultsSelector.map((icon) => {
+        const IconComponent = icon.Icon;
+        return (
+          <div
+            key={icon.pascal_name}
+            onClick={() => setIconName(icon.pascal_name)}
+            className={`cursor-pointer btn p-1 box-border bg-base-100 border-none w-full ${
+              icon.pascal_name === iconName
+                ? "outline outline-1 outline-primary"
+                : ""
+            }`}
+          >
+            <IconComponent size={32} weight={weight} color={color} />
+          </div>
+        );
+      })}
+    </>
+  );
 };
 
 export default IconGrid;
