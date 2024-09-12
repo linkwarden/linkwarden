9c4babb94972f17ae753cfdb8521f490d18d7334
${NEW_NAME} <${NEW_EMAIL}>
2024-09-12 17:00:26 -0400

    add spanish translation

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   i18n: {
     defaultLocale: "en",
-    locales: ["en", "it", "fr", "zh", "uk", "pt-BR", "ja"],
+    locales: ["en", "it", "fr", "zh", "uk", "pt-BR", "ja", "es"],
   },
   reloadOnPrerender: process.env.NODE_ENV === "development",
 };
