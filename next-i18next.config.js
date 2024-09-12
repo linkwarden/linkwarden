b992dd459fac7db3838c6061b0d74f00618bc9ed
spiicytuna <334199+spiicytuna@users.noreply.github.com>
2024-09-12 16:30:20 -0400

    add locale to config

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   i18n: {
     defaultLocale: "en",
-    locales: ["en", "it", "fr", "zh"],
+    locales: ["en", "it", "fr", "zh", "uk"],
   },
   reloadOnPrerender: process.env.NODE_ENV === "development",
 };
