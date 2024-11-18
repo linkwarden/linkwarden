17f2198de24fa8c8f253ef971673e4b8d8d4b610
spiicytuna <334199+spiicytuna@users.noreply.github.com>
2024-11-18 17:53:49 +0100

    Made importBookmarks reusable

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -18,6 +18,7 @@ import Links from "@/components/LinkViews/Links";
 import useLocalSettingsStore from "@/store/localSettings";
 import { useUpdateUser, useUser } from "@/hooks/store/user";
 import SurveyModal from "@/components/ModalContent/SurveyModal";
+import importBookmarks from "@/lib/client/importBookmarks"
 
 export default function Dashboard() {
   const { t } = useTranslation();
@@ -72,70 +73,6 @@ export default function Dashboard() {
       return 2;
     }
   }, []);
-
-  const importBookmarks = async (
-    e: React.ChangeEvent<HTMLInputElement>,
-    format: MigrationFormat
-  ) => {
-    const file: File | null = e.target.files && e.target.files[0];
-
-    if (file) {
-      const reader = new FileReader();
-      //reader.readAsText(file, "UTF-8");
-      reader.readAsArrayBuffer(file);
-      reader.onload = async function (e) {
-        const load = toast.loading("Importing...");
-
-        // const request: string = e.target?.result as string;
-
-        // const body: MigrationRequest = {
-        //   format,
-        //   data: request,
-        // };
-
-        try {
-          const migrationFormatName = MigrationFormat[format];
-          const response = await fetch(`/api/v1/migration/${migrationFormatName}`, {
-            method: "POST",
-            body: e.target?.result as ArrayBuffer,
-            headers: { "Content-Type": "application/octet-stream" },
-          });
-
-          if (!response.ok) {
-            const errorData = await response.json();
-            toast.dismiss(load);
-
-            toast.error(
-              errorData.response ||
-              "Failed to import bookmarks. Please try again."
-            );
-            return;
-          }
-
-          await response.json();
-          toast.dismiss(load);
-          toast.success("Imported the Bookmarks! Reloading the page...");
-
-          setTimeout(() => {
-            location.reload();
-          }, 2000);
-        } catch (error) {
-          console.error("Request failed", error);
-          toast.dismiss(load);
-          toast.error(
-            "An error occurred while importing bookmarks. Please check the logs for more info."
-          );
-        }
-      };
-
-      reader.onerror = function (e) {
-        console.log("Error reading file:", e);
-        toast.error(
-          "Failed to read the file. Please make sure the file is correct and try again."
-        );
-      };
-    }
-  };
 
   const [newLinkModal, setNewLinkModal] = useState(false);
 
--- pages/settings/account.tsx
+++ pages/settings/account.tsx
@@ -18,6 +18,7 @@ import { useTranslation } from "next-i18next";
 import getServerSideProps from "@/lib/client/getServerSideProps";
 import { useUpdateUser, useUser } from "@/hooks/store/user";
 import { z } from "zod";
+import importBookmarks from "@/lib/client/importBookmarks"
 
 const emailEnabled = process.env.NEXT_PUBLIC_EMAIL_PROVIDER;
 
@@ -31,17 +32,19 @@ export default function Account() {
     !objectIsEmpty(account)
       ? account
       : ({
-          id: null,
-          name: "",
-          username: "",
-          email: "",
-          emailVerified: null,
-          password: undefined,
-          image: "",
-          isPrivate: false,
-          createdAt: null,
-          whitelistedUsers: [],
-        } as unknown as AccountSettings)
+        // @ts-ignore
+        id: null,
+        name: "",
+        username: "",
+        email: "",
+        emailVerified: null,
+        password: undefined,
+        image: "",
+        isPrivate: true,
+        // @ts-ignore
+        createdAt: null,
+        whitelistedUsers: [],
+      } as unknown as AccountSettings)
   );
 
   const { t } = useTranslation();
@@ -121,67 +124,6 @@ export default function Account() {
       setTimeout(() => {
         location.reload();
       }, 1000);
-    }
-  };
-
-  const importBookmarks = async (
-    e: React.ChangeEvent<HTMLInputElement>,
-    format: MigrationFormat
-  ) => {
-    const file: File | null = e.target.files && e.target.files[0];
-
-    if (file) {
-      const reader = new FileReader();
-      reader.readAsText(file, "UTF-8");
-      reader.onload = async function (e) {
-        const load = toast.loading("Importing...");
-
-        const request: string = e.target?.result as string;
-
-        const body: MigrationRequest = {
-          format,
-          data: request,
-        };
-
-        try {
-          const response = await fetch("/api/v1/migration", {
-            method: "POST",
-            body: JSON.stringify(body),
-          });
-
-          if (!response.ok) {
-            const errorData = await response.json();
-            toast.dismiss(load);
-
-            toast.error(
-              errorData.response ||
-                "Failed to import bookmarks. Please try again."
-            );
-            return;
-          }
-
-          await response.json();
-          toast.dismiss(load);
-          toast.success("Imported the Bookmarks! Reloading the page...");
-
-          setTimeout(() => {
-            location.reload();
-          }, 2000);
-        } catch (error) {
-          console.error("Request failed", error);
-          toast.dismiss(load);
-          toast.error(
-            "An error occurred while importing bookmarks. Please check the logs for more info."
-          );
-        }
-      };
-
-      reader.onerror = function (e) {
-        console.log("Error reading file:", e);
-        toast.error(
-          "Failed to read the file. Please make sure the file is correct and try again."
-        );
-      };
     }
   };
 
