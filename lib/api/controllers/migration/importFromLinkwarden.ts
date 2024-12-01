4b923541b0b834fcbfd64118da073d26665164d0
spiicytuna <334199+spiicytuna@users.noreply.github.com>
2024-12-01 06:25:13 -0500

    increase the url limits

--- lib/api/controllers/migration/importFromLinkwarden.ts
+++ lib/api/controllers/migration/importFromLinkwarden.ts
@@ -63,7 +63,7 @@ export default async function importFromLinkwarden(
 
             await prisma.link.create({
               data: {
-                url: link.url?.trim(),
+                url: link.url?.trim().slice(0, 2047),
                 name: link.name?.trim().slice(0, 254),
                 description: link.description?.trim().slice(0, 254),
                 importDate: new Date(link.importDate || link.createdAt),
