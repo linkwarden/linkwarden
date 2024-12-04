7136fe6b14a8684482c1c14192b2b1f1f3899a6b
${NEW_NAME} <${NEW_EMAIL}>
2024-12-04 16:56:26 -0600

    format

--- components/ModalContent/DeleteRssSubscriptionModal.tsx
+++ components/ModalContent/DeleteRssSubscriptionModal.tsx
@@ -7,50 +7,54 @@ import { RssSubscription } from "@prisma/client";
 import { useDeleteRssSubscription } from "@/hooks/store/rss";
 
 type Props = {
-	onClose: Function;
-	rssSubscription: RssSubscription;
+  onClose: Function;
+  rssSubscription: RssSubscription;
 };
 
-export default function DeleteRssSubscriptionModal({ onClose, rssSubscription }: Props) {
-	const { t } = useTranslation();
-	const [subscription, setSubscription] = useState<RssSubscription>(rssSubscription);
-	const deleteRssSubscription = useDeleteRssSubscription();
+export default function DeleteRssSubscriptionModal({
+  onClose,
+  rssSubscription,
+}: Props) {
+  const { t } = useTranslation();
+  const [subscription, setSubscription] =
+    useState<RssSubscription>(rssSubscription);
+  const deleteRssSubscription = useDeleteRssSubscription();
 
-	useEffect(() => {
-		setSubscription(rssSubscription);
-	}, []);
+  useEffect(() => {
+    setSubscription(rssSubscription);
+  }, []);
 
-	const submit = async () => {
-		const load = toast.loading(t("deleting"));
+  const submit = async () => {
+    const load = toast.loading(t("deleting"));
 
-		await deleteRssSubscription.mutateAsync(subscription.id, {
-			onSettled: (_, error) => {
-				toast.dismiss(load);
+    await deleteRssSubscription.mutateAsync(subscription.id, {
+      onSettled: (_, error) => {
+        toast.dismiss(load);
 
-				if (error) {
-					toast.error(error.message);
-				} else {
-					onClose();
-					toast.success(t("rss_subscription_deleted"));
-				}
-			},
-		});
-	};
+        if (error) {
+          toast.error(error.message);
+        } else {
+          onClose();
+          toast.success(t("rss_subscription_deleted"));
+        }
+      },
+    });
+  };
 
-	return (
-		<Modal toggleModal={onClose}>
-			<p className="text-xl font-thin text-red-500">{t("delete_link")}</p>
+  return (
+    <Modal toggleModal={onClose}>
+      <p className="text-xl font-thin text-red-500">{t("delete_link")}</p>
 
-			<div className="divider mb-3 mt-1"></div>
+      <div className="divider mb-3 mt-1"></div>
 
-			<div className="flex flex-col gap-3">
-				<p>{t("rss_deletion_confirmation")}</p>
+      <div className="flex flex-col gap-3">
+        <p>{t("rss_deletion_confirmation")}</p>
 
-				<Button className="ml-auto" intent="destructive" onClick={submit}>
-					<i className="bi-trash text-xl" />
-					{t("delete")}
-				</Button>
-			</div>
-		</Modal>
-	);
+        <Button className="ml-auto" intent="destructive" onClick={submit}>
+          <i className="bi-trash text-xl" />
+          {t("delete")}
+        </Button>
+      </div>
+    </Modal>
+  );
 }
