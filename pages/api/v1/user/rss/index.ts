e9eb854c89aefeb024eb05f10361ab5de11c78bb
spiicytuna <334199+spiicytuna@users.noreply.github.com>
2024-12-04 16:56:26 -0600

    format

--- pages/api/v1/user/rss/index.ts
+++ pages/api/v1/user/rss/index.ts
@@ -4,55 +4,62 @@ import verifyUser from "@/lib/api/verifyUser";
 import { UsersAndCollections } from "@prisma/client";
 import { NextApiRequest, NextApiResponse } from "next";
 
-
 export default async function handler(
-	req: NextApiRequest,
-	res: NextApiResponse
+  req: NextApiRequest,
+  res: NextApiResponse
 ) {
-	const user = await verifyUser({ req, res });
-	if (!user) return;
+  const user = await verifyUser({ req, res });
+  if (!user) return;
 
-	if (req.method === "GET") {
-		const response = await prisma.rssSubscription.findMany({
-			include: {
-				collection: {
-					select: {
-						name: true,
-					}
-				},
-			},
-		});
+  if (req.method === "GET") {
+    const response = await prisma.rssSubscription.findMany({
+      include: {
+        collection: {
+          select: {
+            name: true,
+          },
+        },
+      },
+    });
 
-		return res.status(200).json({ response });
-	}
+    return res.status(200).json({ response });
+  }
 
-	if (req.method === "POST") {
-		const { name, url, collectionId } = req.body;
+  if (req.method === "POST") {
+    const { name, url, collectionId } = req.body;
 
-		const collectionIsAccessible = await getPermission({ userId: user.id, collectionId: Number(collectionId) });
+    const collectionIsAccessible = await getPermission({
+      userId: user.id,
+      collectionId: Number(collectionId),
+    });
 
-		const memberHasAccess = collectionIsAccessible?.members.some(
-			(e: UsersAndCollections) => e.userId === user.id
-		);
+    const memberHasAccess = collectionIsAccessible?.members.some(
+      (e: UsersAndCollections) => e.userId === user.id
+    );
 
-		if (collectionIsAccessible?.ownerId !== user.id && !memberHasAccess) {
-			return res.status(403).json({ response: "You do not have permission to add a link to this collection" });
-		}
+    if (collectionIsAccessible?.ownerId !== user.id && !memberHasAccess) {
+      return res
+        .status(403)
+        .json({
+          response:
+            "You do not have permission to add a link to this collection",
+        });
+    }
 
-		const response = await prisma.rssSubscription.create({
-			data: {
-				name,
-				url,
-				ownerId: user.id,
-				lastBuildDate: new Date(),
-				collection: {
-					connect: {
-						id: collectionId,
-					},
-				}
-			},
-		});
+    const response = await prisma.rssSubscription.create({
+      data: {
+        name,
+        url,
+        ownerId: user.id,
+        lastBuildDate: new Date(),
+        collection: {
+          connect: {
+            id: collectionId,
+          },
+        },
+      },
+    });
 
-		return res.status(200).json({ response });
-	}
+    return res.status(200).json({ response });
+  }
 }
--- pages/settings/rss-subscriptions.tsx
+++ pages/settings/rss-subscriptions.tsx
@@ -8,81 +8,84 @@ import { RssSubscription } from "@prisma/client";
 import Link from "next/link";
 
 export default function RssSubscriptions() {
-	const { t } = useTranslation();
-	const { data: rssSubscriptions = [] } = useRssSubscriptions();
+  const { t } = useTranslation();
+  const { data: rssSubscriptions = [] } = useRssSubscriptions();
 
-	const [deleteSubscriptionModal, setDeleteSubscriptionModal] = useState(false);
-	const [selectedSubscription, setSelectedSubscription] = useState<RssSubscription | null>(null);
+  const [deleteSubscriptionModal, setDeleteSubscriptionModal] = useState(false);
+  const [selectedSubscription, setSelectedSubscription] =
+    useState<RssSubscription | null>(null);
 
-	const openDeleteModal = (subscription: RssSubscription) => {
-		setSelectedSubscription(subscription);
-		setDeleteSubscriptionModal(true);
-	};
+  const openDeleteModal = (subscription: RssSubscription) => {
+    setSelectedSubscription(subscription);
+    setDeleteSubscriptionModal(true);
+  };
 
-	return (
-		<SettingsLayout>
-			<p className="capitalize text-3xl font-thin inline">
-				{t("rss_subscriptions")}
-			</p>
+  return (
+    <SettingsLayout>
+      <p className="capitalize text-3xl font-thin inline">
+        {t("rss_subscriptions")}
+      </p>
 
-			<div className="divider my-3"></div>
-			<div className="flex flex-col gap-3">
-				<p>{t("rss_subscriptions_desc")}</p>
+      <div className="divider my-3"></div>
+      <div className="flex flex-col gap-3">
+        <p>{t("rss_subscriptions_desc")}</p>
 
-				<button
-					className={`btn ml-auto btn-accent dark:border-violet-400 text-white tracking-wider w-fit flex items-center gap-2`}
-				// onClick={() => {
-				// 	setNewTokenModal(true);
-				// }}
-				>
-					{t("new_rss_subscription")}
-				</button>
-				{rssSubscriptions.length > 0 && (
-					<table className="table mt-2 overflow-x-auto">
-						<thead>
-							<tr>
-								<th>{t("name")}</th>
-								<th>{t("link")}</th>
-								<th>{t("collection")}</th>
-								<th></th>
-							</tr>
-						</thead>
-						<tbody>
-							{rssSubscriptions.map((rssSubscription, i) => {
-								return (
-									<tr key={i}>
-										<td>{rssSubscription.name}</td>
-										<td>{rssSubscription.url}</td>
-										<td>
-											<Link className="hover:underline" href={`/collections/${rssSubscription.collectionId}`}>{rssSubscription.collection.name}</Link>
-										</td>
-										<td>
-											<button
-												className="btn btn-sm btn-ghost btn-square hover:bg-red-500"
-												onClick={() => openDeleteModal(rssSubscription)}
-
-											>
-												<i className="bi-x text-lg"></i>
-											</button>
-										</td>
-									</tr>
-								);
-							})}
-						</tbody>
-					</table>
-				)}
-			</div>
-			{deleteSubscriptionModal && selectedSubscription && (
-				<DeleteRssSubscriptionModal
-					onClose={() => {
-						setDeleteSubscriptionModal(false);
-						setSelectedSubscription(null);
-					}}
-					rssSubscription={selectedSubscription}
-				/>
-			)}
-		</SettingsLayout>
-	);
+        <button
+          className={`btn ml-auto btn-accent dark:border-violet-400 text-white tracking-wider w-fit flex items-center gap-2`}
+          // onClick={() => {
+          // 	setNewTokenModal(true);
+          // }}
+        >
+          {t("new_rss_subscription")}
+        </button>
+        {rssSubscriptions.length > 0 && (
+          <table className="table mt-2 overflow-x-auto">
+            <thead>
+              <tr>
+                <th>{t("name")}</th>
+                <th>{t("link")}</th>
+                <th>{t("collection")}</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {rssSubscriptions.map((rssSubscription, i) => (
+                <tr key={i}>
+                  <td>{rssSubscription.name}</td>
+                  <td>{rssSubscription.url}</td>
+                  <td>
+                    <Link
+                      className="hover:underline"
+                      href={`/collections/${rssSubscription.collectionId}`}
+                    >
+                      {rssSubscription.collection.name}
+                    </Link>
+                  </td>
+                  <td>
+                    <button
+                      className="btn btn-sm btn-ghost btn-square hover:bg-red-500"
+                      onClick={() => openDeleteModal(rssSubscription)}
+                    >
+                      <i className="bi-x text-lg"></i>
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
+      </div>
+      {deleteSubscriptionModal && selectedSubscription && (
+        <DeleteRssSubscriptionModal
+          rssSubscription={selectedSubscription}
+          onClose={() => {
+            setDeleteSubscriptionModal(false);
+            setSelectedSubscription(null);
+          }}
+        />
+      )}
+    </SettingsLayout>
+  );
 }
 
 export { getServerSideProps };
