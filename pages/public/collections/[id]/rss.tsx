eb87788e65b91de25d1489ff60a83d28481aae00
spiicytuna <334199+spiicytuna@users.noreply.github.com>
2024-12-19 16:06:24 -0600

    Format & Lint

--- pages/public/collections/[id]/rss.tsx
+++ pages/public/collections/[id]/rss.tsx
@@ -1,72 +1,77 @@
-import React from 'react'
-import { GetServerSideProps } from 'next'
-import { prisma } from '@/lib/api/db'
-import RSS from 'rss'
+import React from "react";
+import { GetServerSideProps } from "next";
+import { prisma } from "@/lib/api/db";
+import RSS from "rss";
 
-const RSSFeed: React.FC = () => null
+const RSSFeed: React.FC = () => null;
 
-export const getServerSideProps: GetServerSideProps = async ({ params, res, req }) => {
-	const id = parseInt(params?.id as string, 10);
-	if (isNaN(id)) {
-		if (res) {
-			res.statusCode = 404
-			res.end()
-		}
-		return { props: {} }
-	}
+export const getServerSideProps: GetServerSideProps = async ({
+  params,
+  res,
+  req,
+}) => {
+  const id = parseInt(params?.id as string, 10);
+  if (isNaN(id)) {
+    if (res) {
+      res.statusCode = 404;
+      res.end();
+    }
+    return { props: {} };
+  }
 
-	const data = await prisma.collection.findUnique({
-		where: {
-			id: parseInt(params?.id as string),
-			isPublic: true,
-		},
-		include: {
-			links: {
-				orderBy: {
-					createdAt: 'desc',
-				},
-				take: 20,
-			}
-		},
-	})
+  const data = await prisma.collection.findUnique({
+    where: {
+      id: parseInt(params?.id as string),
+      isPublic: true,
+    },
+    include: {
+      links: {
+        orderBy: {
+          createdAt: "desc",
+        },
+        take: 20,
+      },
+    },
+  });
 
-	if (!data) {
-		res.statusCode = 404;
-		res.end('Not Found');
-		return { props: {} };
-	}
+  if (!data) {
+    res.statusCode = 404;
+    res.end("Not Found");
+    return { props: {} };
+  }
 
-	const host = req.headers.host;
+  const host = req.headers.host;
 
-	const protocol = req.headers['x-forwarded-proto'] || (req.socket ? 'https' : 'http');
-	const siteUrl = `${protocol}://${host}/public/collections/${data.id}`;
-	const feedUrl = `${protocol}://${host}/public/collections/${data.id}/rss`;
+  const protocol =
+    req.headers["x-forwarded-proto"] || (req.socket ? "https" : "http");
+  const siteUrl = `${protocol}://${host}/public/collections/${data.id}`;
+  const feedUrl = `${protocol}://${host}/public/collections/${data.id}/rss`;
 
-	const feed = new RSS({
-		title: data.name,
-		description: data.description,
-		site_url: siteUrl,
-		feed_url: feedUrl,
-	})
+  const feed = new RSS({
+    title: data.name,
+    description: data.description,
+    site_url: siteUrl,
+    feed_url: feedUrl,
+  });
 
-	data.links.forEach((link) => {
-		feed.item({
-			title: link.name,
-			description: link.description,
-			url: link.url || '',
-			date: link.createdAt,
-		})
-	})
+  data.links.forEach((link) => {
+    feed.item({
+      title: link.name,
+      description: link.description,
+      url: link.url || "",
+      date: link.createdAt,
+    });
+  });
 
-	const xml = feed.xml({ indent: true });
+  const xml = feed.xml({ indent: true });
 
-	if (res) {
-		res.setHeader('Content-Type', 'text/xml')
-		res.write(xml)
-		res.end()
-	}
+  if (res) {
+    res.setHeader("Content-Type", "text/xml");
+    res.write(xml);
+    res.end();
+  }
 
-	return { props: {} }
-}
+  return { props: {} };
+};
 
-export default RSSFeed
+export default RSSFeed;
