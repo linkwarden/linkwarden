4de4e95f32f4ea7d81d2cb84d10e60b6a0819601
AverageHelper <me@average.name>
2024-12-29 16:42:31 -0700

    feat: Remove Himalaya in favor of jsdom

--- lib/api/controllers/migration/importFromHTMLFile.ts
+++ lib/api/controllers/migration/importFromHTMLFile.ts
@@ -1,8 +1,6 @@
 import { prisma } from "@/lib/api/db";
 import createFolder from "@/lib/api/storage/createFolder";
 import { JSDOM } from "jsdom";
-import { decodeHTML, DecodingMode } from "entities";
-import { parse, Node, Element, TextNode } from "himalaya";
 import { hasPassedLimit } from "../../verifyCapacity";
 
 export default async function importFromHTMLFile(
@@ -29,12 +27,10 @@ export default async function importFromHTMLFile(
     };
   }
 
-  const jsonData = parse(document.documentElement.outerHTML);
+  processNodes(document);
 
-  const processedArray = processNodes(jsonData);
-
-  for (const item of processedArray) {
-    await processBookmarks(userId, item as Element);
+  for (const item of document.children) {
+    await processBookmarks(userId, item);
   }
 
   return { response: "Success.", status: 200 };
@@ -42,105 +38,83 @@ export default async function importFromHTMLFile(
 
 async function processBookmarks(
   userId: number,
-  data: Node,
+  data: Element,
   parentCollectionId?: number
 ) {
-  if (data.type === "element") {
-    for (const item of data.children) {
-      if (item.type === "element" && item.tagName === "dt") {
-        // process collection or sub-collection
+  for (const item of data.children) {
+    if (item.tagName === "DT") {
+      // process collection or sub-collection
 
-        let collectionId;
-        const collectionName = item.children.find(
-          (e) => e.type === "element" && e.tagName === "h3"
-        ) as Element;
+      let collectionId: number | undefined;
+      const collectionName = item.getElementsByTagName("h3")[0];
 
-        if (collectionName) {
-          const collectionNameContent = (collectionName.children[0] as TextNode)
-            ?.content;
-          if (collectionNameContent) {
-            collectionId = await createCollection(
-              userId,
-              collectionNameContent,
-              parentCollectionId
-            );
-          } else {
-            // Handle the case when the collection name is empty
-            collectionId = await createCollection(
-              userId,
-              "Untitled Collection",
-              parentCollectionId
-            );
-          }
-        }
-        await processBookmarks(
-          userId,
-          item,
-          collectionId || parentCollectionId
-        );
-      } else if (item.type === "element" && item.tagName === "a") {
-        // process link
-
-        const rawLinkUrl = item?.attributes.find(
-          (e) => e.key.toLowerCase() === "href"
-        )?.value;
-        const linkUrl = decodeEntities(rawLinkUrl);
-        const linkName = (
-          item?.children.find((e) => e.type === "text") as TextNode
-        )?.content;
-        const linkTags = item?.attributes
-          .find((e) => e.key === "tags")
-          ?.value.split(",")
-          .map(decodeEntities);
-
-        // set date if available
-        const linkDateValue = item?.attributes.find(
-          (e) => e.key.toLowerCase() === "add_date"
-        )?.value;
-
-        const linkDate = linkDateValue
-          ? new Date(Number(linkDateValue) * 1000)
-          : undefined;
-
-        let linkDesc =
-          (
-            (
-              item?.children?.find(
-                (e) => e.type === "element" && e.tagName === "dd"
-              ) as Element
-            )?.children[0] as TextNode
-          )?.content || "";
-
-        if (linkUrl && parentCollectionId) {
-          await createLink(
+      if (collectionName) {
+        const collectionNameContent = collectionName?.textContent;
+        if (collectionNameContent) {
+          collectionId = await createCollection(
             userId,
-            linkUrl,
-            parentCollectionId,
-            linkName,
-            linkDesc,
-            linkTags,
-            linkDate
+            collectionNameContent,
+            parentCollectionId
           );
-        } else if (linkUrl) {
-          // create a collection named "Imported Bookmarks" and add the link to it
-          const collectionId = await createCollection(userId, "Imports");
-
-          await createLink(
+        } else {
+          // Handle the case when the collection name is empty
+          collectionId = await createCollection(
             userId,
-            linkUrl,
-            collectionId,
-            linkName,
-            linkDesc,
-            linkTags,
-            linkDate
+            "Untitled Collection",
+            parentCollectionId
           );
         }
-
-        await processBookmarks(userId, item, parentCollectionId);
-      } else {
-        // process anything else
-        await processBookmarks(userId, item, parentCollectionId);
       }
+      await processBookmarks(userId, item, collectionId || parentCollectionId);
+    } else if (item.tagName === "A") {
+      // process link
+
+      const linkUrl = item?.getAttribute("href");
+      const linkName =
+        (
+          item?.childNodes &&
+          Array.from(item.childNodes).find((e) => e.nodeType === 3)
+        )?.nodeValue ?? undefined;
+      const linkTags = item?.getAttribute("tags")?.split(",");
+
+      // set date if available
+      const linkDateValue = item?.getAttribute("add_date");
+
+      const linkDate = linkDateValue
+        ? new Date(Number(linkDateValue) * 1000)
+        : undefined;
+
+      const linkDesc = item?.getElementsByTagName("dd")[0]?.textContent || "";
+
+      if (linkUrl && parentCollectionId) {
+        await createLink(
+          userId,
+          linkUrl,
+          parentCollectionId,
+          linkName,
+          linkDesc,
+          linkTags,
+          linkDate
+        );
+      } else if (linkUrl) {
+        // create a collection named "Imported Bookmarks" and add the link to it
+        const collectionId = await createCollection(userId, "Imports");
+
+        await createLink(
+          userId,
+          linkUrl,
+          collectionId,
+          linkName,
+          linkDesc,
+          linkTags,
+          linkDate
+        );
+      }
+
+      await processBookmarks(userId, item, parentCollectionId);
+    } else {
+      // process anything else
+      await processBookmarks(userId, item, parentCollectionId);
     }
   }
 }
@@ -224,75 +198,64 @@ const createLink = async (
       description,
       collectionId,
       createdById: userId,
-      tags:
-        tags && tags[0]
-          ? {
-              connectOrCreate: tags.map((tag: string) => {
-                return {
-                  where: {
-                    name_ownerId: {
-                      name: tag.trim(),
-                      ownerId: userId,
-                    },
-                  },
-                  create: {
+      tags: tags?.[0]
+        ? {
+            connectOrCreate: tags.map((tag: string) => {
+              return {
+                where: {
+                  name_ownerId: {
                     name: tag.trim(),
-                    owner: {
-                      connect: {
-                        id: userId,
-                      },
+                    ownerId: userId,
+                  },
+                },
+                create: {
+                  name: tag.trim(),
+                  owner: {
+                    connect: {
+                      id: userId,
                     },
                   },
-                };
-              }),
-            }
-          : undefined,
+                },
+              };
+            }),
+          }
+        : undefined,
       importDate: importDate || undefined,
     },
   });
 };
 
-function processNodes(nodes: Node[]) {
-  const findAndProcessDL = (node: Node) => {
-    if (node.type === "element" && node.tagName === "dl") {
+function processNodes(document: Document): void {
+  const findAndProcessDL = (node: Element) => {
+    if (node.tagName === "DL") {
       processDLChildren(node);
-    } else if (
-      node.type === "element" &&
-      node.children &&
-      node.children.length
-    ) {
-      node.children.forEach((child) => findAndProcessDL(child));
+    } else if (node.children?.length) {
+      for (const child of node.children) {
+        findAndProcessDL(child);
+      }
     }
   };
 
   const processDLChildren = (dlNode: Element) => {
-    dlNode.children.forEach((child, i) => {
-      if (child.type === "element" && child.tagName === "dt") {
+    for (let i = 0; i < dlNode.children.length; i += 1) {
+      const child = dlNode.children[i];
+      if (child?.tagName === "DT") {
         const nextSibling = dlNode.children[i + 1];
-        if (
-          nextSibling &&
-          nextSibling.type === "element" &&
-          nextSibling.tagName === "dd"
-        ) {
-          const aElement = child.children.find(
-            (el) => el.type === "element" && el.tagName === "a"
-          );
-          if (aElement && aElement.type === "element") {
+        if (nextSibling?.tagName === "DD") {
+          const aElement = child.getElementsByTagName("a")[0];
+          if (aElement) {
             // Add the 'dd' element as a child of the 'a' element
-            aElement.children.push(nextSibling);
+            aElement.appendChild(nextSibling);
             // Remove the 'dd' from the parent 'dl' to avoid duplicate processing
-            dlNode.children.splice(i + 1, 1);
+            dlNode.removeChild(nextSibling);
             // Adjust the loop counter due to the removal
           }
         }
       }
-    });
+    }
   };
 
-  nodes.forEach(findAndProcessDL);
-  return nodes;
-}
-
-function decodeEntities(encoded: string | undefined): string {
-  return decodeHTML(encoded ?? "", DecodingMode.Attribute);
+  for (const node of document.children) {
+    findAndProcessDL(node);
+  }
 }
