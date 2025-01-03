09f00effaf27658b73dbc015ca3f5104848fe5f8
spiicytuna <334199+spiicytuna@users.noreply.github.com>
2025-01-02 23:16:03 -0500

    minor change

--- lib/shared/schemaValidation.ts
+++ lib/shared/schemaValidation.ts
@@ -100,6 +100,7 @@ export const PostLinkSchema = z.object({
   url: z.string().trim().max(2048).url().optional(),
   name: z.string().trim().max(2048).optional(),
   description: z.string().trim().max(2048).optional(),
+  image: z.enum(["jpeg", "png"]).optional(),
   collection: z
     .object({
       id: z.number().optional(),
