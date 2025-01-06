fe6eedd895ab36d3c4bdcfa6ada0ca54818ebf80
spiicytuna <334199+spiicytuna@users.noreply.github.com>
2025-01-06 11:32:44 -0500

    use SSR for public ENV

---
+++ pages/api/v1/config/index.ts
@@ -0,0 +1,21 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import * as process from "process";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  res.json(getEnvData());
+}
+
+export const getEnvData = () => {
+  return {
+    DISABLE_REGISTRATION:
+      process.env.NEXT_PUBLIC_DISABLE_REGISTRATION === "true" || undefined,
+    ADMIN: Number(process.env.NEXT_PUBLIC_ADMIN) || undefined,
+    RSS_POLLING_INTERVAL_MINUTES:
+      Number(process.env.NEXT_PUBLIC_RSS_POLLING_INTERVAL_MINUTES) || undefined,
+    EMAIL_PROVIDER:
+      process.env.NEXT_PUBLIC_EMAIL_PROVIDER === "true" || undefined,
+    MAX_FILE_BUFFER:
+      Number(process.env.NEXT_PUBLIC_MAX_FILE_BUFFER) || undefined,
+    AI_ENABLED: process.env.AI_ENABLED === "true" || undefined,
+  };
+};
