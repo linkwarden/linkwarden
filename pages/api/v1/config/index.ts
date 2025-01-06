bd9c6f125257d320e247e165151ea6d5fbf298b4
${NEW_NAME} <${NEW_EMAIL}>
2025-01-06 12:39:16 -0500

    bug fix

--- pages/api/v1/config/index.ts
+++ pages/api/v1/config/index.ts
@@ -2,20 +2,18 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import * as process from "process";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.json(getEnvData());
+  return res.status(200).json({ response: getEnvData() });
 }
 
 export const getEnvData = () => {
   return {
     DISABLE_REGISTRATION:
-      process.env.NEXT_PUBLIC_DISABLE_REGISTRATION === "true" || undefined,
-    ADMIN: Number(process.env.NEXT_PUBLIC_ADMIN) || undefined,
+      process.env.NEXT_PUBLIC_DISABLE_REGISTRATION === "true" || null,
+    ADMIN: Number(process.env.NEXT_PUBLIC_ADMIN) || null,
     RSS_POLLING_INTERVAL_MINUTES:
-      Number(process.env.NEXT_PUBLIC_RSS_POLLING_INTERVAL_MINUTES) || undefined,
-    EMAIL_PROVIDER:
-      process.env.NEXT_PUBLIC_EMAIL_PROVIDER === "true" || undefined,
-    MAX_FILE_BUFFER:
-      Number(process.env.NEXT_PUBLIC_MAX_FILE_BUFFER) || undefined,
-    AI_ENABLED: process.env.AI_ENABLED === "true" || undefined,
+      Number(process.env.NEXT_PUBLIC_RSS_POLLING_INTERVAL_MINUTES) || null,
+    EMAIL_PROVIDER: process.env.NEXT_PUBLIC_EMAIL_PROVIDER === "true" || null,
+    MAX_FILE_BUFFER: Number(process.env.NEXT_PUBLIC_MAX_FILE_BUFFER) || null,
+    AI_ENABLED: process.env.AI_ENABLED === "true" || null,
   };
 };
