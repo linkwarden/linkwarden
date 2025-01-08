c520724fa07443d03eab59b89a10cdad25225e6a
Antony Denyer <email@antonydenyer.co.uk>
2025-01-08 15:50:55 +0000

    Add readable content into rss feed

--- pages/public/collections/[id]/rss.tsx
+++ pages/public/collections/[id]/rss.tsx
@@ -1,16 +1,23 @@
 import React from "react";
 import { GetServerSideProps } from "next";
 import { prisma } from "@/lib/api/db";
+import { ArchivedFormat } from "@/types/global";
+import readFile from "@/lib/api/storage/readFile";
+
 import RSS from "rss";
+import { json } from "stream/consumers";
 
 const RSSFeed: React.FC = () => null;
 
 export const getServerSideProps: GetServerSideProps = async ({
   params,
+  query,
   res,
   req,
 }) => {
   const id = Number(params?.id as string);
+  const format = Number(query?.format as string)
+
   if (isNaN(id) || !id) {
     if (res) {
       res.statusCode = 404;
@@ -52,16 +59,52 @@ export const getServerSideProps: GetServerSideProps = async ({
     description: data.description,
     site_url: siteUrl,
     feed_url: feedUrl,
+    custom_namespaces: {
+      'media': "http://search.yahoo.com/mrss/",
+    },
+    custom_elements: [
+      { 'content:encoded': 'encoded' },
+      { 'media:thumbnail': 'thumbnail' }
+    ]
   });
 
-  data.links.forEach((link) => {
-    feed.item({
+
+  for (const link of data.links) {
+    const item = {
       title: link.name,
       description: link.description,
-      url: link.url || "",
+      url: link.url,
       date: link.createdAt,
-    });
-  });
+      enclosure: {
+        url: `${protocol}://${host}/public/preserved/${link.id}?format=${ArchivedFormat.pdf}`,
+        type: "application/pdf"
+      }
+    } as RSS.ItemOptions
+
+    const { file, status } = await readFile(
+      `archives/${data.id}/${link.id}_readability.json`
+    );
+
+    if (status === 200) {
+      const result = JSON.parse(file.toString())
+
+      item.description = result.excerpt || link.description
+      item.custom_elements = [{
+        'content:encoded': `<![CDATA[${result.content}]]>`,
+        'media.content': {
+          url: `${protocol}://${host}/public/preserved/${link.id}?format=${ArchivedFormat.pdf}`,
+          type: 'application/pdf'
+        },
+        'media.thumbnail': {
+          url: `${protocol}://${host}/api/v1/archives/${link.id}?format=${ArchivedFormat.jpeg}&preview=true`,
+          type: 'media/jpeg'
+        }
+      }]
+    }
+
+    feed.item(item)
+  }
+
 
   const xml = feed.xml({ indent: true });
 
