2e17b234ac3adf06e4beb93178829e13ab2d19ef
${NEW_NAME} <${NEW_EMAIL}>
2025-01-27 02:08:12 -0500

    improvements

--- components/ModalContent/LinkModal.tsx
+++ components/ModalContent/LinkModal.tsx
@@ -1,16 +1,6 @@
-import React, { useState } from "react";
 import { LinkIncludingShortenedCollectionAndTags } from "@/types/global";
-import { useTranslation } from "next-i18next";
-import { useDeleteLink } from "@/hooks/store/links";
 import LinkDetails from "../LinkDetails";
-import Link from "next/link";
-import usePermissions from "@/hooks/usePermissions";
-import { useRouter } from "next/router";
-import { dropdownTriggerer } from "@/lib/client/utils";
-import toast from "react-hot-toast";
-import clsx from "clsx";
 import Modal from "../Modal";
-import useWindowDimensions from "@/hooks/useWindowDimensions";
 
 type Props = {
   onClose: Function;
@@ -29,27 +19,16 @@ export default function LinkModal({
   link,
   activeMode,
 }: Props) {
-  const { t } = useTranslation();
-
-  const permissions = usePermissions(link.collection.id as number);
-
-  const router = useRouter();
-
-  const isPublicRoute = router.pathname.startsWith("/public") ? true : false;
-
-  const deleteLink = useDeleteLink();
-
-  const [mode, setMode] = useState<"view" | "edit">(activeMode || "view");
-
   return (
     <Modal toggleModal={onClose} isLinkModal>
       <LinkDetails
         activeLink={link}
         className="sm:mt-0 -mt-11"
-        mode={mode}
-        setMode={(mode: "view" | "edit") => setMode(mode)}
         onUpdateArchive={onUpdateArchive}
         onClose={onClose}
+        activeMode={activeMode}
+        onDelete={onDelete}
+        onPin={onPin}
       />
     </Modal>
   );
