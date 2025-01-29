5f12046a49419238532c1165e5f6acb5a3e7e029
Isaac <mail@isaacwise.com>
2025-01-28 19:26:52 -0600

    Provide list of models for openai & anthropic

--- lib/api/autoTagLink.ts
+++ lib/api/autoTagLink.ts
@@ -58,7 +58,7 @@ export default async function autoTagLink(user: User, linkId: number) {
   })
 
   try {
-    let tags: string[] = object || [];
+    let tags = object
 
     console.log("Tags generated for link: ", link.url, tags);
 
