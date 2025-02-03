616efffed23e59dbc6a25ddaa3e85cbe3865f4bc
daniel31x13 <daniel31x13@gmail.com>
2025-02-03 06:57:22 -0500

    improvements

--- lib/api/prompts.ts
+++ lib/api/prompts.ts
@@ -2,7 +2,6 @@ export const generateTagsPrompt = (text: string) => `
 You are a Bookmark Manager that should extract relevant tags from the following text, here are the rules:
 - The final output should be only an array of tags.
 - The tags should be in the language of the text.
-- Every word in a tag must start with a capital letter.
 - The maximum number of tags is 5.
 - Each tag should be maximum one to two words.
 - If there are no tags, return an empty array.
@@ -18,7 +17,6 @@ Predefined tags: ${tags.join(", ")}.
 Here are the rules:
 - The final output should be only an array of tags.
 - The tags should be in the language of the text.
-- Every word in a tag must start with a capital letter.
 - The maximum number of tags is 5.
 - Each tag should be maximum one to two words.
 - If there are no tags, return an empty array.
@@ -34,7 +32,6 @@ The existing tags are sorted from most used to least used: ${tags.join(", ")}.
 Here are the rules:
 - The final output should be only an array of tags.
 - The tags should be in the language of the text.
-- Every word in a tag must start with a capital letter.
 - The maximum number of tags is 5.
 - Each tag should be maximum one to two words.
 - If there are no tags, return an empty array.
