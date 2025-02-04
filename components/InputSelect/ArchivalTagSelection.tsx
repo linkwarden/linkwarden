18dfbb2b64ce03508a25cdf18ca0c83df23e52ae
spiicytuna <334199+spiicytuna@users.noreply.github.com>
2025-02-04 17:06:43 -0600

    remove duplicate functions

--- components/InputSelect/ArchivalTagSelection.tsx
+++ components/InputSelect/ArchivalTagSelection.tsx
@@ -2,27 +2,6 @@ import CreatableSelect from "react-select/creatable";
 import { styles } from "./styles";
 import { useTranslation } from "next-i18next";
 import { ArchivalTagOption } from "./types";
-import { Tag } from "@prisma/client";
-
-
-export const transformTag = (tag: Tag) => ({
-	label: tag.name,
-	value: tag.id,
-	archiveAsScreenshot: tag.archiveAsScreenshot || false,
-	archiveAsMonolith: tag.archiveAsMonolith || false,
-	archiveAsPDF: tag.archiveAsPDF || false,
-	archiveAsReadable: tag.archiveAsReadable || false,
-	archiveAsWaybackMachine: tag.archiveAsWaybackMachine || false,
-	aiTag: tag.aiTag || false,
-});
-
-export const isArchivalTag = (tag: Tag) =>
-	tag.archiveAsScreenshot ||
-	tag.archiveAsMonolith ||
-	tag.archiveAsPDF ||
-	tag.archiveAsReadable ||
-	tag.archiveAsWaybackMachine ||
-	tag.aiTag;
 
 type Props = {
 	options: ArchivalTagOption[] | []
