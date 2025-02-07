aec26c554e653ab1da6dea6c17f325a0915e4f4e
spiicytuna <334199+spiicytuna@users.noreply.github.com>
2025-02-06 21:21:18 -0600

    Start worker archival update/delete options

--- pages/api/v1/links/archive/index.ts
+++ pages/api/v1/links/archive/index.ts
@@ -1,7 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import verifyUser from "@/lib/api/verifyUser";
 import { prisma } from "@/lib/api/db";
-import { DeleteLinksArchiveSchema, PutLinksArchiveSchema } from "@/lib/shared/schemaValidation";
+import { LinkArchiveActionSchema } from "@/lib/shared/schemaValidation";
 import { removeFiles } from "@/lib/api/manageLinkFiles";
 
 export default async function links(req: NextApiRequest, res: NextApiResponse) {
@@ -14,82 +14,8 @@ export default async function links(req: NextApiRequest, res: NextApiResponse) {
 		return res.status(401).json({ response: "Permission denied." });
 	}
 
-	if (req.method === "PUT") {
-		const dataValidation = PutLinksArchiveSchema.safeParse(req.body);
-		if (!dataValidation.success) {
-			return res.status(400).json({
-				response: `Error: ${dataValidation.error.issues[0].message
-					} [${dataValidation.error.issues[0].path.join(", ")}]`,
-			});
-		}
-
-		const { links } = dataValidation.data;
-
-		if (links && links.length > 0) {
-			const brokenArchives = await prisma.link.findMany({
-				where: {
-					id: {
-						in: links,
-					},
-					OR: [
-						{ image: "unavailable" },
-						{ pdf: "unavailable" },
-						{ readable: "unavailable" },
-						{ monolith: "unavailable" },
-						{ preview: "unavailable" },
-					],
-				},
-			});
-
-			for (const link of brokenArchives) {
-				await removeFiles(link.id, link.collectionId)
-				await prisma.link.update({
-					where: {
-						id: link.id,
-					},
-					data: {
-						image: null,
-						pdf: null,
-						readable: null,
-						monolith: null,
-						preview: null,
-					}
-				})
-			}
-		} else {
-			const brokenArchives = await prisma.link.findMany({
-				where: {
-					OR: [
-						{ image: "unavailable" },
-						{ pdf: "unavailable" },
-						{ readable: "unavailable" },
-						{ monolith: "unavailable" },
-					],
-				},
-			});
-
-			for (const link of brokenArchives) {
-				await removeFiles(link.id, link.collectionId)
-				await prisma.link.update({
-					where: {
-						id: link.id,
-					},
-					data: {
-						image: null,
-						pdf: null,
-						readable: null,
-						monolith: null,
-						preview: null,
-					}
-				})
-			}
-		}
-
-		return res.status(200).json({ response: "Broken archives deleted." });
-	}
-
 	if (req.method === "DELETE") {
-		const dataValidation = DeleteLinksArchiveSchema.safeParse(req.body);
+		const dataValidation = LinkArchiveActionSchema.safeParse(req.body);
 		if (!dataValidation.success) {
 			return res.status(400).json({
 				response: `Error: ${dataValidation.error.issues[0].message
@@ -99,7 +25,7 @@ export default async function links(req: NextApiRequest, res: NextApiResponse) {
 
 		const { action } = dataValidation.data;
 
-		if (action === 'delete') {
+		if (action === "allAndIgnore") {
 			const allLinks = await prisma.link.findMany();
 
 			for (const link of allLinks) {
@@ -118,8 +44,8 @@ export default async function links(req: NextApiRequest, res: NextApiResponse) {
 				})
 			}
 
-			return res.status(200).json({ response: "All archives deleted" });
-		} else if (action === 're-preserve') {
+			return res.status(200).json({ response: "Success." });
+		} else if (action === "allAndRePreserve") {
 			const allLinks = await prisma.link.findMany();
 
 			for (const link of allLinks) {
@@ -137,7 +63,46 @@ export default async function links(req: NextApiRequest, res: NextApiResponse) {
 					}
 				})
 			}
-			return res.status(200).json({ response: "All archives deleted and are re-preserving" });
+
+			return res.status(200).json({ response: "Success." });
+		} else if (action === "allBroken") {
+			const brokenArchives = await prisma.link.findMany({
+				where: {
+					OR: [
+						{ image: "unavailable" },
+						{ pdf: "unavailable" },
+						{ readable: "unavailable" },
+						{ monolith: "unavailable" },
+						{ preview: "unavailable" },
+					],
+				},
+				include: {
+					createdBy: {
+						select: {
+							archiveAsScreenshot: true,
+							archiveAsMonolith: true,
+							archiveAsPDF: true,
+							archiveAsReadable: true,
+							archiveAsWaybackMachine: true,
+						}
+					},
+					tags: {
+						select: {
+							archiveAsScreenshot: true,
+							archiveAsMonolith: true,
+							archiveAsPDF: true,
+							archiveAsReadable: true,
+							archiveAsWaybackMachine: true,
+						}
+					}
+				}
+			});
+
+			// Need to check tags & user options for archival formats
+			// and then if the link is missing the same excluded formats
+			// then we can ignore it
+
+			return res.status(200).json({ response: "Success." });
 		}
 	}
 }
