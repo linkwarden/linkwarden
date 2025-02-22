baea0337d9c7b83dc6bd532d8b02d03514853d1f
spiicytuna <334199+spiicytuna@users.noreply.github.com>
2025-02-22 10:22:42 -0500

    add negative queries

--- lib/api/searchQueryBuilder.ts
+++ lib/api/searchQueryBuilder.ts
@@ -1,65 +1,79 @@
 interface Token {
   field: string;
   value: string;
+  isNegative: boolean;
 }
+
+const SEARCH_CONDITIONS = [
+  "url",
+  "name",
+  "description",
+  "type",
+  "collection",
+  "pinned",
+  "public",
+  "before",
+  "after",
+  "tag",
+];
 
 export function parseSearchTokens(searchQueryString: string): Token[] {
   // Split on whitespace
-  const rawTokens = searchQueryString.match(/\S+/g) || [];
+  const rawTokens =
+    searchQueryString.match(/\\?.|^$/g)?.reduce(
+      (p, c) => {
+        if (c === '"' || c === "'") {
+          p.quote ^= 1;
+        } else if (!p.quote && c === " ") {
+          p.str.push("");
+        } else {
+          p.str[p.str.length - 1] += c.replace(/\\(.)/, "$1");
+        }
+        return p;
+      },
+      { str: [""], quote: 0 }
+    ).str || [];
   const tokens: Token[] = [];
 
-  for (const token of rawTokens) {
-    if (token.startsWith("url:") && token.length > "url:".length) {
-      tokens.push({ field: "url", value: token.substring("url:".length) });
-    } else if (token.startsWith("name:") && token.length > "name:".length) {
-      tokens.push({ field: "name", value: token.substring("name:".length) });
-    } else if (
-      token.startsWith("description:") &&
-      token.length > "description:".length
-    ) {
-      tokens.push({
-        field: "description",
-        value: token.substring("description:".length),
-      });
-    } else if (token.startsWith("type:") && token.length > "type:".length) {
-      tokens.push({ field: "type", value: token.substring("type:".length) });
-    } else if (token.startsWith("before:") && token.length > "before:".length) {
-      tokens.push({
-        field: "before",
-        value: token.substring("before:".length),
-      });
-    } else if (token.startsWith("after:") && token.length > "after:".length) {
-      if (token.startsWith("after:")) {
-        tokens.push({
-          field: "after",
-          value: token.substring("after:".length),
-        });
+  for (let token of rawTokens) {
+    let isNegative = false;
+
+    if (token.startsWith("!") && token.length > 1) {
+      const valueAfterNegation = token.substring(1);
+
+      if (
+        SEARCH_CONDITIONS.some((field) =>
+          valueAfterNegation.startsWith(`${field}:`)
+        )
+      ) {
+        isNegative = true;
+        token = valueAfterNegation;
       }
-    } else if (
-      token.startsWith("collection:") &&
-      token.length > "collection:".length
-    ) {
-      tokens.push({
-        field: "collection",
-        value: token.substring("collection:".length),
-      });
-    } else if (token === "pinned:true" || token === "pinned:false") {
-      tokens.push({
-        field: "pinned",
-        value: token.substring("pinned:".length).toLowerCase(),
-      });
-    } else if (token.startsWith("#") && token.length > 1) {
-      tokens.push({ field: "tag", value: token.substring(1) });
-    } else if (token === "public:true") {
-      tokens.push({
-        field: "public",
-        value: token.substring("public:".length).toLowerCase(),
-      });
-    } else {
+    }
+
+    let match = false;
+
+    for (const field of SEARCH_CONDITIONS) {
+      if (token.startsWith(`${field}:`) && token.length > `${field}:`.length) {
+        tokens.push({
+          field,
+          value: token.substring(`${field}:`.length),
+          isNegative,
+        });
+        match = true;
+        break;
+      }
+    }
+
+    if (!match) {
       // everything else -> 'general' text
-      tokens.push({ field: "general", value: token });
+      tokens.push({ field: "general", value: token, isNegative });
     }
   }
+
+  const SEARCH_QUERY_LIMIT = Number(process.env.SEARCH_QUERY_LIMIT);
+
+  if (SEARCH_QUERY_LIMIT) return tokens.slice(0, SEARCH_QUERY_LIMIT);
 
   return tokens;
 }
@@ -77,55 +91,107 @@ export function buildMeiliFilters(tokens: Token[], userId: number): string[] {
     `(collectionOwnerId = ${userId}) OR (collectionMemberIds = ${userId})`,
   ];
 
-  for (const { field, value } of tokens) {
+  for (const { field, value, isNegative } of tokens) {
     switch (field) {
       case "url":
-        filters.push(`url = "${escapeForMeilisearch(value)}"`);
+        filters.push(
+          isNegative
+            ? `NOT url = "${escapeForMeilisearch(value)}"`
+            : `url = "${escapeForMeilisearch(value)}"`
+        );
         break;
+
       case "name":
-        filters.push(`name = "${escapeForMeilisearch(value)}"`);
+        filters.push(
+          isNegative
+            ? `NOT name = "${escapeForMeilisearch(value)}"`
+            : `name = "${escapeForMeilisearch(value)}"`
+        );
         break;
+
       case "description":
-        filters.push(`description = "${escapeForMeilisearch(value)}"`);
+        filters.push(
+          isNegative
+            ? `NOT description = "${escapeForMeilisearch(value)}"`
+            : `description = "${escapeForMeilisearch(value)}"`
+        );
         break;
+
       case "type":
-        filters.push(`type = "${escapeForMeilisearch(value)}"`);
+        filters.push(
+          isNegative
+            ? `NOT type = "${escapeForMeilisearch(value)}"`
+            : `type = "${escapeForMeilisearch(value)}"`
+        );
         break;
+
       case "collection":
-        filters.push(`collectionName = "${escapeForMeilisearch(value)}"`);
+        filters.push(
+          isNegative
+            ? `NOT collectionName = "${escapeForMeilisearch(value)}"`
+            : `collectionName = "${escapeForMeilisearch(value)}"`
+        );
         break;
+
       case "pinned":
         if (value === "true") {
-          filters.push(`pinnedBy = ${userId}`);
+          filters.push(
+            isNegative ? `NOT pinnedBy = ${userId}` : `pinnedBy = ${userId}`
+          );
         } else if (value === "false") {
-          filters.push(`NOT pinnedBy = ${userId}`);
+          filters.push(
+            isNegative ? `pinnedBy = ${userId}` : `NOT pinnedBy = ${userId}`
+          );
         }
         break;
+
       case "public":
         if (value === "true") {
-          filters.push(`collectionIsPublic = true`);
+          filters.push(
+            isNegative
+              ? `NOT collectionIsPublic = true`
+              : `collectionIsPublic = true`
+          );
         }
         break;
+
       case "before":
         if (!isNaN(Date.parse(value))) {
           const creationTimestamp = Date.parse(value) / 1000;
-          filters.push(`creationTimestamp < ${creationTimestamp}`);
+          filters.push(
+            isNegative
+              ? `creationTimestamp >= ${creationTimestamp}`
+              : `creationTimestamp < ${creationTimestamp}`
+          );
         }
         break;
+
       case "after":
         if (!isNaN(Date.parse(value))) {
           const creationTimestamp = Date.parse(value) / 1000;
-          filters.push(`creationTimestamp > ${creationTimestamp}`);
+          filters.push(
+            isNegative
+              ? `creationTimestamp <= ${creationTimestamp}`
+              : `creationTimestamp > ${creationTimestamp}`
+          );
         }
         break;
+
       case "tag":
-        filters.push(`tags = "${escapeForMeilisearch(value)}"`);
+        filters.push(
+          isNegative
+            ? `NOT tags = "${escapeForMeilisearch(value)}"`
+            : `tags = "${escapeForMeilisearch(value)}"`
+        );
         break;
+
       case "general":
       default:
+        // "general" text is handled by the main query, not by filters
         break;
     }
   }
+
   return filters;
 }
 
