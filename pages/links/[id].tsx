d60a2fd73811a98cadfb93437c22653ebcc3ab00
${NEW_NAME} <${NEW_EMAIL}>
2025-03-03 18:01:02 -0500

    revert modal

--- pages/links/[id].tsx
+++ pages/links/[id].tsx
@@ -19,9 +19,13 @@ const Index = () => {
   return (
     <div className="flex h-screen">
       {getLink.data ? (
-        <LinkDetails activeLink={getLink.data} standalone />
+        <LinkDetails
+          activeLink={getLink.data}
+          className="sm:max-w-xl sm:m-auto sm:p-5 w-full"
+          standalone
+        />
       ) : (
-        <div className="sm:max-w-3xl p-5 m-auto w-full flex flex-col items-center gap-5">
+        <div className="max-w-xl p-5 m-auto w-full flex flex-col items-center gap-5">
           <div className="w-20 h-20 skeleton rounded-xl"></div>
           <div className="w-full h-10 skeleton rounded-xl"></div>
           <div className="w-full h-10 skeleton rounded-xl"></div>
