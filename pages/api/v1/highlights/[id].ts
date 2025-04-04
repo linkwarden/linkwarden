6599fd7f5ae05f0854f0ba5553377debe715c2a0
daniel31x13 <daniel31x13@gmail.com>
2025-04-04 11:29:46 -0400

    minor fix

--- pages/api/v1/highlights/[id].ts
+++ pages/api/v1/highlights/[id].ts
@@ -10,6 +10,12 @@ export default async function highlights(
   if (!user) return;
 
   if (req.method === "DELETE") {
+    if (process.env.NEXT_PUBLIC_DEMO === "true")
+      return res.status(400).json({
+        response:
+          "This action is disabled because this is a read-only demo of Linkwarden.",
+      });
+
     const highlights = await deleteHighlightById(
       user.id,
       Number(req.query.id as string)
