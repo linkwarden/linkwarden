linkwarden/linkwarden

commit aa0c7d64f4943e9d225f58f6e389a345a235364b

bug fixes

layouts/MainLayout.tsx
@@ -42,7 +42,7 @@ export default function MainLayout({ children }: Props) {
       </div>
 
       <div
-        className={`w-full sm:pb-0 pb-20 flex flex-col min-h-screen lg:ml-80`}
+        className={`lg:w-[calc(100%-320px)] overflow-x-hidden w-full sm:pb-0 pb-20 flex flex-col min-h-screen lg:ml-80`}
       >
         <Navbar />
         {children}

components/LinkViews/LinkComponents/LinkList.tsx
@@ -105,7 +105,7 @@ export default function LinkCardCompact({ link, editMode }: Props) {
       <div
         className={`${selectedStyle} rounded-md border relative group items-center flex ${
           !isPWA() ? "hover:bg-base-300 px-2 py-1" : "py-1"
-        } duration-200 w-full`}
+        } duration-200`}
         onClick={() =>
           selectable
             ? handleCheckboxClick(link)

pages/api/v1/rss/index.ts
@@ -48,7 +48,7 @@ export default async function handler(
     const RSS_SUBSCRIPTION_LIMIT_PER_USER =
       Number(process.env.RSS_SUBSCRIPTION_LIMIT_PER_USER) || 20;
 
-    if (rssSubscriptionCount >= 10) {
+    if (rssSubscriptionCount >= RSS_SUBSCRIPTION_LIMIT_PER_USER) {
       return res.status(403).json({
         response: `You have reached the limit of ${RSS_SUBSCRIPTION_LIMIT_PER_USER} RSS subscriptions.`,
       });

commit 49ae195fe87990178eb3bd619ec16207a8475ba2

(fix) MainLayout nested scrolling when ViewMode.List
(I think) this commit aa0c7d6 introduced a bug where, when `ViewMode === List`, nested (vertical) scrollbars appear and scrolling initially incorrectly scrolls the outer content before then scrolling the inner content.

afaict `overflow-x-hidden` is not needed (I can't see any horizontal scrollbars without it) and removing it fixes the issue.

layouts/MainLayout.tsx
@@ -42,7 +42,7 @@ export default function MainLayout({ children }: Props) {
       </div>
 
       <div
-        className={`lg:w-[calc(100%-320px)] overflow-x-hidden w-full sm:pb-0 pb-20 flex flex-col min-h-screen lg:ml-80`}
+        className={`lg:w-[calc(100%-320px)] w-full sm:pb-0 pb-20 flex flex-col min-h-screen lg:ml-80`}
       >
         <Navbar />
         {children}