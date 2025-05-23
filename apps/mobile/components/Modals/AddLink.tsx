da695530b793c3c42b0d4955699fa2df7167f075
spiicytuna <334199+spiicytuna@users.noreply.github.com>
2025-05-23 10:23:38 -0400

    feat(mobile): add modal component

--- apps/mobile/components/Modals/AddLink.tsx
+++ apps/mobile/components/Modals/AddLink.tsx
@@ -1,49 +1,17 @@
-import { Modal, View, Text, Pressable, StyleSheet } from "react-native";
 import { PropsWithChildren } from "react";
 import { IconSymbol } from "../ui/IconSymbol";
+import ModalBase from "../ModalBase";
+import { Text } from "react-native";
 
 type Props = PropsWithChildren<{
   isVisible: boolean;
   onClose: () => void;
 }>;
 
-export default function AddLink({ isVisible, children, onClose }: Props) {
+export default function AddLink({ isVisible, onClose }: Props) {
   return (
-    <View>
-      <Modal animationType="slide" transparent={true} visible={isVisible}>
-        <View
-          style={{
-            flex: 1,
-            justifyContent: "flex-end",
-            backgroundColor: "rgba(0, 0, 0, 0.5)",
-          }}
-        >
-          <View
-            style={{
-              flexDirection: "row",
-              justifyContent: "space-between",
-              alignItems: "center",
-              padding: 20,
-              backgroundColor: "#fff",
-            }}
-          >
-            <Text
-              style={{
-                fontSize: 20,
-                fontWeight: "600",
-                color: "#000",
-                marginBottom: 10,
-              }}
-            >
-              Choose a sticker
-            </Text>
-            <Pressable onPress={onClose}>
-              <IconSymbol name="xmark" color="" size={22} />
-            </Pressable>
-          </View>
-          {children}
-        </View>
-      </Modal>
-    </View>
+    // <ModalBase isVisible={isVisible} onClose={onClose}>
+    <Text>Hi</Text>
+    // </ModalBase>
   );
 }
