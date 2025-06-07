06d8cf3cb8ef296b543b8793df9dc86132a615de
Isaac <mail@isaacwise.com>
2025-06-07 18:29:19 -0500

    format

--- apps/web/components/CollectionListing.tsx
+++ apps/web/components/CollectionListing.tsx
@@ -297,10 +297,11 @@ const renderItem = (
   return (
     <div ref={provided.innerRef} {...provided.draggableProps} className="mb-1">
       <div
-        className={`${currentPath === `/collections/${collection.id}`
+        className={`${
+          currentPath === `/collections/${collection.id}`
             ? "bg-primary/20 is-active"
             : "hover:bg-neutral/20"
-          } duration-100 flex gap-1 items-center pr-2 pl-1 rounded-md`}
+        } duration-100 flex gap-1 items-center pr-2 pl-1 rounded-md`}
       >
         {Dropdown(item as ExtendedTreeItem, onExpand, onCollapse)}
 
