67a31649e9c6a5ab1c6f7c5c0aae3fd0ad2adc5d
${NEW_NAME} <${NEW_EMAIL}>
2025-06-07 19:05:42 -0500

    fix building

--- apps/web/lib/api/controllers/users/userId/getUserById.ts
+++ apps/web/lib/api/controllers/users/userId/getUserById.ts
@@ -1,5 +1,17 @@
 import { prisma } from "@linkwarden/prisma";
-import { Subscription, User, WhitelistedUser } from "@linkwarden/prisma/client";
+import { Subscription, User } from "@linkwarden/prisma/client";
+
+type GetUserByIdResponse = Omit<User, "password"> &
+  Partial<{ subscription: Pick<Subscription, "active" | "quantity"> }> & {
+    parentSubscription: {
+      active: boolean | undefined;
+      user: {
+        email: string | null | undefined;
+      };
+    };
+  } & {
+    whitelistedUsers: string[];
+  }
 
 export default async function getUserById(userId: number) {
   const user = await prisma.user.findUnique({
@@ -32,12 +44,12 @@ export default async function getUserById(userId: number) {
   const { password, subscriptions, parentSubscription, ...lessSensitiveInfo } =
     user;
 
-  const data = {
+  const data: GetUserByIdResponse = {
     ...lessSensitiveInfo,
     whitelistedUsers: whitelistedUsernames,
     subscription: {
-      active: subscriptions?.active,
-      quantity: subscriptions?.quantity,
+      active: subscriptions?.active ?? false,
+      quantity: subscriptions?.quantity ?? 0,
     },
     parentSubscription: {
       active: parentSubscription?.active,
@@ -45,17 +57,7 @@ export default async function getUserById(userId: number) {
         email: parentSubscription?.user.email,
       },
     },
-  } as Omit<User, "password"> &
-    Partial<{ subscription: Subscription }> & {
-      parentSubscription: {
-        active: boolean | undefined;
-        user: {
-          email: string | null | undefined;
-        };
-      };
-    } & {
-      whitelistedUsers: string[];
-    };
+  }
 
   return { response: data, status: 200 };
 }
