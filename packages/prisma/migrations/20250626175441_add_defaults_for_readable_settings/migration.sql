126d98d1b8d31ebe96be1710d5ae6cebea0e96fe
daniel31x13 <daniel31x13@gmail.com>
2025-06-26 13:54:50 -0400

    small improvement

---
+++ packages/prisma/migrations/20250626175441_add_defaults_for_readable_settings/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "User" ALTER COLUMN "readableFontFamily" SET DEFAULT 'sans-serif',
+ALTER COLUMN "readableFontSize" SET DEFAULT '20px',
+ALTER COLUMN "readableLineHeight" SET DEFAULT '1.8',
+ALTER COLUMN "readableLineWidth" SET DEFAULT 'normal';
