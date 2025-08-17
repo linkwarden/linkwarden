f3590e595061368c770aff027f99c7c286bb69fb
spiicytuna <334199+spiicytuna@users.noreply.github.com>
2025-08-17 07:18:28 -0400

    use new api

--- apps/web/lib/api/stripe/checkSubscriptionByEmail.ts
+++ apps/web/lib/api/stripe/checkSubscriptionByEmail.ts
@@ -1,31 +1,29 @@
-import Stripe from "stripe";
+import stripeSDK from "./stripeSDK";
 
 const STRIPE_SECRET_KEY = process.env.STRIPE_SECRET_KEY;
 
 export default async function checkSubscriptionByEmail(email: string) {
   if (!STRIPE_SECRET_KEY) return null;
 
-  const stripe = new Stripe(STRIPE_SECRET_KEY, {
-    apiVersion: "2022-11-15",
-  });
+  const stripe = stripeSDK();
 
   console.log("Request made to Stripe by:", email);
-  const listByEmail = await stripe.customers.list({
+  const customers = await stripe.customers.list({
     email: email.toLowerCase(),
-    expand: ["data.subscriptions"],
+    expand: ["data.subscriptions.data.items"],
   });
 
-  if (listByEmail?.data[0]?.subscriptions?.data[0]) {
-    return {
-      active: (listByEmail.data[0].subscriptions?.data[0] as any).plan.active,
-      stripeSubscriptionId: listByEmail.data[0].subscriptions?.data[0].id,
-      currentPeriodStart:
-        listByEmail.data[0].subscriptions?.data[0].current_period_start * 1000,
-      currentPeriodEnd:
-        listByEmail.data[0].subscriptions?.data[0].current_period_end * 1000,
-      quantity: (listByEmail?.data[0]?.subscriptions?.data[0] as any).quantity,
-    };
-  } else {
-    return null;
-  }
+  const sub = customers.data[0]?.subscriptions?.data?.[0];
+  if (!sub) return null;
+
+  const item = sub.items.data[0];
+  if (!item) return null;
+
+  return {
+    active: sub.status === "active" || sub.status === "trialing",
+    stripeSubscriptionId: sub.id,
+    currentPeriodStart: item.current_period_start * 1000,
+    currentPeriodEnd: item.current_period_end * 1000,
+    quantity: item.quantity ?? 1,
+  };
 }
