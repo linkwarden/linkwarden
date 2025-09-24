6a40a70e6d13521331272e62ef387a595fd2132b
daniel31x13 <daniel31x13@gmail.com>
2025-09-24 15:32:36 -0400

    small improvement

--- apps/web/components/SidebarHighlightLink.tsx
+++ apps/web/components/SidebarHighlightLink.tsx
@@ -1,27 +1,56 @@
 import Link from "next/link";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { cn } from "@/lib/utils";
 
 export default function SidebarHighlightLink({
   title,
   href,
   icon,
   active,
+  sidebarIsCollapsed,
 }: {
   title: string;
   href: string;
   icon: string;
   active?: boolean;
+  sidebarIsCollapsed?: boolean;
 }) {
   return (
-    <Link href={href}>
-      <div
-        title={title}
-        className={`${
-          active || false ? "bg-primary/20" : "hover:bg-neutral/20"
-        } duration-200 px-3 py-1 cursor-pointer flex items-center gap-2 w-full rounded-lg capitalize`}
-      >
-        <i className={`${icon} text-primary text-xl drop-shadow`}></i>
-        <p className="truncate w-full font-semibold text-sm">{title}</p>
-      </div>
-    </Link>
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <Link href={href} title={title}>
+            <div
+              className={cn(
+                active ? "bg-primary/20" : "hover:bg-neutral/20",
+                "duration-200 cursor-pointer flex items-center gap-2 capitalize",
+                sidebarIsCollapsed
+                  ? "rounded-md h-8 w-8"
+                  : "rounded-lg px-3 py-1"
+              )}
+            >
+              <i
+                className={cn(
+                  icon,
+                  "text-primary text-xl drop-shadow",
+                  sidebarIsCollapsed && "w-full text-center"
+                )}
+              ></i>
+              {!sidebarIsCollapsed && (
+                <p className="truncate w-full font-semibold text-sm">{title}</p>
+              )}
+            </div>
+          </Link>
+        </TooltipTrigger>
+        {sidebarIsCollapsed && (
+          <TooltipContent side="right">{title}</TooltipContent>
+        )}
+      </Tooltip>
+    </TooltipProvider>
   );
 }
