dd4c925a56fd49e234dbb214d571a38f78094623
daniel31x13 <daniel31x13@gmail.com>
2025-11-22 20:30:55 -0500

    remove unnecessary plugin

--- apps/mobile/plugins/file-sharing.js
+++
@@ -1,7 +0,0 @@
-module.exports = function withFileSharing(config) {
-  config.ios = config.ios || {};
-  config.ios.infoPlist = config.ios.infoPlist || {};
-  config.ios.infoPlist.UIFileSharingEnabled = true;
-  config.ios.infoPlist.LSSupportsOpeningDocumentsInPlace = true;
-  return config;
-};
