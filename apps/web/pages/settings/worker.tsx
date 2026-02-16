1f478cdbcfab498d3f6fdf1d3712d3b995dcb700
aanno <aanno@users.noreply.github.com>
2026-02-16 11:17:58 +0100

    fixed worker

--- apps/web/pages/settings/worker.tsx
+++ apps/web/pages/settings/worker.tsx
@@ -1,17 +1,7 @@
-import type { GetServerSideProps } from "next";
 import getServerSideProps from "@/lib/client/getServerSideProps";
-
-export const getServerSideProps: GetServerSideProps = async () => {
-  return {
-    redirect: {
-      destination: "/admin/background-jobs",
-      permanent: false,
-    },
-  };
-
-export { getServerSideProps };
-};
 
 export default function WorkerRedirect() {
   return null;
 }
+
+export { getServerSideProps };
