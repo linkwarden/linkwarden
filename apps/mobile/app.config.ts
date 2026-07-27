95fad858f22de25feab2c4645aa6904d595e70ae
daniel31x13 <daniel31x13@gmail.com>
2026-07-27 13:38:49 -0400

    mobile: bump version

--- apps/mobile/app.config.ts
+++ apps/mobile/app.config.ts
@@ -3,7 +3,7 @@ import { ExpoConfig } from "expo/config";
 const config: ExpoConfig = {
   name: "Linkwarden",
   slug: "linkwarden",
-  version: "1.3.1",
+  version: "1.4.0",
   orientation: "portrait",
   icon: "./assets/images/icon.png",
   scheme: "linkwarden",
