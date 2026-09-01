e3e68ff9a7cbef1861ebac2e6262b9308b28d578
daniel31x13 <daniel31x13@gmail.com>
2026-09-01 17:07:28 -0400

    fix(extension): fix tailwind auto-completion

--- apps/extension/tailwind.config.js
+++ apps/extension/tailwind.config.js
@@ -1,12 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ["class"],
-  content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-	],
+  content: ["./src/**/*.{ts,tsx}"],
   theme: {
     container: {
       center: true,
@@ -73,4 +68,4 @@ module.exports = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-}
+};
