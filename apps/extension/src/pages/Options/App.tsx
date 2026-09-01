fc6276fc1dae3e5241dd11aa0005b48321163597
daniel31x13 <daniel31x13@gmail.com>
2026-09-01 15:52:26 -0400

    small change

--- apps/extension/src/pages/Options/App.tsx
+++ apps/extension/src/pages/Options/App.tsx
@@ -1,7 +1,7 @@
-import WholeContainer from '../../@/components/WholeContainer.tsx';
-import Container from '../../@/components/Container.tsx';
-import { Separator } from '../../@/components/ui/Separator.tsx';
-import OptionsForm from '../../@/components/OptionsForm.tsx';
+import WholeContainer from "../../@/components/WholeContainer.tsx";
+import Container from "../../@/components/Container.tsx";
+import { Separator } from "../../@/components/ui/Separator.tsx";
+import OptionsForm from "../../@/components/OptionsForm.tsx";
 
 const App = () => {
   return (
@@ -12,18 +12,6 @@ const App = () => {
         </div>
         <div>
           <Separator />
-          <p className="text-base pt-2">
-            This is a extension for the{' '}
-            <a
-              href="https://github.com/linkwarden/linkwarden"
-              rel="noopener"
-              target="_blank"
-              className="text-blue-400 hover:text-blue-500 duration-100 hover:underline"
-            >
-              Linkwarden
-            </a>
-            . Please fill out the following text boxes accordingly.
-          </p>
         </div>
         <OptionsForm />
       </Container>
