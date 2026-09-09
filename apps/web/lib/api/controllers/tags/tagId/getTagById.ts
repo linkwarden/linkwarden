ba3c613c267a7e7d4e2989ba1b59f8949726b3cd
daniel31x13 <daniel31x13@gmail.com>
2026-09-09 17:09:20 -0400

    feat(web): improved performance for the dashboard and search route

--- apps/web/lib/api/controllers/tags/tagId/getTagById.ts
+++ apps/web/lib/api/controllers/tags/tagId/getTagById.ts
@@ -1,8 +1,11 @@
 import { prisma } from "@linkwarden/prisma";
+import getAccessibleCollectionIds from "@/lib/api/getAccessibleCollectionIds";
 
 export default async function getTagById(userId: number, tagId: number) {
   if (!tagId)
     return { response: "Please choose a valid name for the tag.", status: 401 };
+
+  const { memberCollectionIds } = await getAccessibleCollectionIds(userId);
 
   const tag = await prisma.tag.findFirst({
     where: {
@@ -12,13 +15,7 @@ export default async function getTagById(userId: number, tagId: number) {
         {
           links: {
             some: {
-              collection: {
-                members: {
-                  some: {
-                    userId,
-                  },
-                },
-              },
+              collectionId: { in: memberCollectionIds },
             },
           },
         },
